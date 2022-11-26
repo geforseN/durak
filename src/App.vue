938f77d651cf72339cf3ea2471bdfadaa92e1dc8
Semen Nebogatov <semen.neb@gmail.com>
2022-11-26 16:01:30 +0700

    add <vue-header> and <main>

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <available-rooms :rooms="mockRoomsData" />
+  <vue-header :profileId="dataFromServer.profileId"></vue-header>
+  <main class="grid grid-cols-12 auto-rows-auto">
+    <available-rooms class="col-start-4 col-span-6" :rooms="mockRoomsData" />
+  </main>
 </template>
 
 <script setup lang="ts">
   import { mockRoomsData } from "@/mock/available-rooms.mock";
   import AvailableRooms from "@/components/available-rooms.vue";
-
+  import VueHeader from "@/components/vue-header.vue";
+  const dataFromServer = {
+    profileId: "qweqwerty123",
+  };
 </script>
 
 <style scoped></style>
