3a021286e7cdbdad2ce6f35dfa41df794d7d41a0
Semen Nebogatov <semen.neb@gmail.com>
2022-11-26 15:59:32 +0700

    add <vue-header> proto

---
+++ src/components/vue-header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <ul class="flex gap-2 px-4 py-1 bg-emerald-200">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <span>Text</span>
+      </li>
+      <li class="ml-auto">
+        <a class="underline" :href="`profile/${props.profileId}`">My Profile</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    profileId: string | number;
+  }>();
+</script>
+
+<style scoped></style>
