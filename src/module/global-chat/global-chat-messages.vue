373664d0ca9e9b1890dd1ccfb0fa69b6ef9bbc98
Semen Nebogatov <semen.neb@gmail.com>
2023-01-14 13:57:36 +0700

    fix double notification

--- src/module/global-chat/global-chat-messages.vue
+++ src/module/global-chat/global-chat-messages.vue
@@ -6,23 +6,16 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import useNotificationStore from "@/stores/notification.store";
-import type { Notification } from "@/stores/notification.store";
-import socket, { globalChat } from "@/socket";
+import { globalChat } from "@/socket";
 import GlobalChatUserMessage from "../global-chat/global-chat-user-message.vue";
 import type { UserMessage } from "@/module/global-chat/types";
 import formatTime from "@/utils/intl/format-time";
 
 const messages = ref<UserMessage[]>([]);
 
-const { addNotificationInQueue } = useNotificationStore();
-
 globalChat.on("server:sendMessage", (messageObject: UserMessage) => {
   messageObject.date = formatTime(messageObject.date as number);
   messages.value.push(messageObject);
 });
 
-socket.on("server:sendNotification", (notification: Notification) => {
-  addNotificationInQueue(notification);
-});
 </script>
