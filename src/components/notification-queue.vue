6bd85319220619ca2ba4216f4bb057ff6cca48d0
Semen Nebogatov <semen.neb@gmail.com>
2023-01-14 20:05:34 +0700

    update notification queue

--- src/components/notification-queue.vue
+++ src/components/notification-queue.vue
@@ -2,8 +2,9 @@
   <div class="fixed top-3 right-[50%] translate-x-[50%]">
     <div v-for="{ type, message, id } of notificationQueue" :id="id">
       <div
-      class="py-2 w-96 rounded-sm border-2 border-black m-2 flex justify-center" 
-      :class="backgroundColorMap[type] || 'bg-gray-500'">
+        class="py-2 w-96 rounded-sm border-2 border-black m-2 flex justify-center"
+        :class="backgroundColorMap[type] || 'bg-gray-500'"
+      >
         {{ message }}
       </div>
     </div>
@@ -13,14 +14,19 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { storeToRefs } from "pinia";
-import socket from "@/socket";
+import socket, { globalChat } from "@/socket";
 import useNotificationStore from "@/stores/notification.store";
 import type { Notification } from "@/stores/notification.store";
 
 const notificationStore = useNotificationStore();
 const { notificationQueue } = storeToRefs(notificationStore);
 
+// for now unused on server
 socket.on("server:sendNotification", (notification: Notification) => {
+  notificationStore.addNotificationInQueue(notification);
+});
+
+globalChat.on("server:sendNotification", (notification: Notification) => {
   notificationStore.addNotificationInQueue(notification);
 });
 
