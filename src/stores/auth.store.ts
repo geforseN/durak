2b2213735ce85268bd6bdce2352a0b4f04536e37
GeForsen- <semen.neb@gmail.com>
2023-01-22 13:36:26 +0700

    update auth store

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -2,19 +2,14 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const useAuthStore = defineStore("auth", () => {
-  const hasAccount = ref(false);
-  const hasForgotCredintials = ref(false);
-
   const email = ref("");
-  const username = ref("");
+  const accName = ref("");
   const password = ref("");
   const repeatedPassword = ref("");
 
   return {
-    hasAccount,
-    hasForgotCredintials,
     email,
-    username,
+    accName,
     password,
     repeatedPassword,
   };
