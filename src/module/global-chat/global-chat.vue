06dfc0b3a3c0d534ccf0eb3c3855d298fbbf31b6
GeForsen- <semen.neb@gmail.com>
2023-01-22 14:08:22 +0700

    global-chat style, error handle update

--- src/module/global-chat/global-chat.vue
+++ src/module/global-chat/global-chat.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="w-[40rem] h-[20rem] flex flex-col justify-between border border-black p-2"
-  >
+  <div class="flex flex-col justify-between border border-black p-2">
     <global-chat-messages />
     <div class="mt-2 grid grid-flow-col grid-cols-[1fr_max-content]">
       <section>
@@ -28,8 +26,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { globalChat } from "@/socket";
-import useNotificationStore from "@/stores/notification.store";
-
+import { useNotificationStore } from "@/stores/notification.store";
 import GlobalChatMessages from "@/module/global-chat/global-chat-messages.vue";
 import generateNotificationFromError from "@/utils/generate-notification-from-error";
 import assertInputLength from "@/utils/assert-input-length";
@@ -43,8 +40,12 @@ const handleMessage = () => {
     globalChat.emit("sendMessage", input.value);
     input.value = "";
   } catch (error) {
-    const notification = generateNotificationFromError(error as Error);
-    addNotificationInQueue(notification);
+    if (error instanceof Error) {
+      const notification = generateNotificationFromError(error);
+      addNotificationInQueue(notification);
+    } else {
+      console.log(error);
+    }
   }
 };
 </script>
