fe3967cae3cf2ec7b0524e481b9f123363e52308
GeForsen- <semen.neb@gmail.com>
2023-03-01 12:20:35 +0700

    add Rank & Suit type in index

--- src/module/card-game/types/index.ts
+++ src/module/card-game/types/index.ts
@@ -1,10 +1,12 @@
-import type { Card } from "./Card";
+import type { Card, Rank, Suit } from "./Card";
 import type { DeskSlot } from "./DeskSlot";
 import type { GameState } from "./GameState";
 import type { Self, Enemy, UIStatus, User, PlayerRole } from "./User";
 
 export {
   Card,
+  Rank,
+  Suit,
   DeskSlot,
   GameState,
   Self,
