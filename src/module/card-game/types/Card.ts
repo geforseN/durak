eb24daa92741da01a0b74b821d668e02c344c0ef
GeForsen- <semen.neb@gmail.com>
2023-03-01 12:18:52 +0700

    add Rank & Suit type in Card

--- src/module/card-game/types/Card.ts
+++ src/module/card-game/types/Card.ts
@@ -1,5 +1,8 @@
 export type Card = {
-  rank: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
-  suit: "♠" | "♦" | "♥" | "♣";
+  rank: Rank;
+  suit: Suit;
   power: number;
 }
+
+export type Rank = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+export type Suit = "♠" | "♦" | "♥" | "♣";
