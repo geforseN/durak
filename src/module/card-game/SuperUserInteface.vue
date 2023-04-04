a3c5acc9e8129ec0f2db3248f904f7129081ba6a
GeForsen- <semen.neb@gmail.com>
2023-04-04 16:01:50 +0700

    add SuperUserInterface component

---
+++ src/module/card-game/SuperUserInteface.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex font-bold justify-evenly">
+    <div v-if="gameUI.attackStatus !== 'hidden'" :class="gameUI.attackStatus === 'freeze' && 'opacity-50'"
+         class="border-2 border-black flex justify-center items-center gap-x-1">
+      <button class="w-full h-full bg-red-600 px-2 " @click="stopMove">
+        Закончить атаку
+      </button>
+    </div>
+    <div v-if="gameUI.defendStatus !== 'hidden'" :class="gameUI.defendStatus === 'freeze' && 'opacity-50'"
+         class="border-2 border-black flex justify-center items-center gap-x-1">
+      <button class="w-full h-full bg-red-600 px-2 " @click="stopMove">
+        Забрать карты
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGameUIStore } from "@/stores/game/gameUI.store";
+
+const gameUI = useGameUIStore();
+const emit = defineEmits<{
+  (e: "stopMove"): void
+}>()
+
+const stopMove = () => emit("stopMove");
+</script>
