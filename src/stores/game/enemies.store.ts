70abcdcff8a0966eea6dbaa1ae2ac840f51e702f
GeForsen- <semen.neb@gmail.com>
2023-04-04 15:25:46 +0700

    add GameEnemies store

---
+++ src/stores/game/enemies.store.ts
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import type { Enemy } from "@/module/card-game/types";
+
+export const useGameEnemiesStore = defineStore("gameEnemies", () => {
+  const enemies = ref<Enemy[]>([]);
+
+  const findBy = ({ accname }: { accname: string }) => {
+    return enemies.value.find((enemy) => enemy.info.accname === accname);
+  };
+
+  const changeEnemyCardCount = (accname: string, cardCount: number) => {
+    const enemy = findBy({ accname });
+    if (!enemy) return alert("No such enemy");
+    enemy.cardCount = cardCount;
+  };
+
+  return { enemies, findBy, changeEnemyCardCount };
+});
