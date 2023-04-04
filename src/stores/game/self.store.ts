4242e1559f3dbd4c10a967a0152aa5b57eb2987a
GeForsen- <semen.neb@gmail.com>
2023-04-04 15:21:00 +0700

    add GameSelf store

---
+++ src/stores/game/self.store.ts
@@ -0,0 +1,33 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import type { PlayerRole, Self, UserInfo, Card } from "@/module/card-game/types";
+
+const defaultUserInfo: UserInfo = {
+  accname: "",
+  personalLink: "",
+  photoUrl: "",
+  nickname: "",
+  connectStatus: "OFFLINE",
+};
+
+export const useGameSelfStore = defineStore("gameSelf", () => {
+  const self = ref<Self>({ cards: [], info: defaultUserInfo, role: "Player" });
+
+  const pushCard = (...cards: Card[]) => {
+    self.value.cards.push(...cards);
+  };
+
+  const removeCard = ({ suit, rank }: Card) => {
+    const cardIndex = self.value.cards.findIndex(
+      (card) => card.suit === suit && card.rank === rank,
+    );
+    if (!cardIndex) return alert("Can not remove card from Self");
+    self.value.cards.splice(cardIndex, 1);
+  };
+
+  const changeRole = (accname: string, role: PlayerRole) => {
+    if (accname === self.value.info.accname) self.value.role = role;
+  };
+
+  return { pushCard, removeCard, changeRole, self };
+});
