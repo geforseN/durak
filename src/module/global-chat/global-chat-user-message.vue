0387ae55edef4616c6517aa63edb9ff535d19cd1
GeForsen- <semen.neb@gmail.com>
2023-04-12 23:54:04 +0700

    GlobalCharUserMessage: change css

--- src/module/global-chat/global-chat-user-message.vue
+++ src/module/global-chat/global-chat-user-message.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="grid grid-flow-col grid-cols-[max-content_1fr_max-content]">
-    <router-link :to="'profile/' + message.sender.personalLink" class="bold block underline flex">
-      <img :src="message.sender.photoUrl" :alt="`${message.sender.nickname} profile picture`" class="max-h-8 aspect-square">
-      {{ message.sender.nickname }}:
-    </router-link>
-    <p class="hyphens ml-1 break-all bg-blue-400">
-      {{ message.text }}
-    </p>
-    <span class="opacity-40 justify-self-end pr-2">
-      {{ message.date }}
-    </span>
+  <div class="chat " :class="sendByUser ? 'chat-end ' : 'chat-start'">
+    <div class="w-12 avatar chat-image indicator" :class="sendByUser ? 'pr-2' : 'pl-2 '">
+      <div class="rounded">
+        <img :src="sender.photoUrl" :alt="`${sender.nickname} profile picture`" />
+      </div>
+    </div>
+    <div class="chat-bubble break-all flex flex-col" :class="sendByUser ? 'items-end' : 'items-start'">
+      <span class="font-bold badge bg-black rounded-sm p-1">{{ sender.nickname }}</span>
+      <p>{{ text }}</p>
+      <time class="text-xs opacity-50">{{ date }}</time>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { UserMessage } from "@/module/global-chat/types";
+import { useUserStore } from "@/stores/user.store";
+import { computed } from "@vue/reactivity";
 
-const { message } = defineProps<{ message: UserMessage }>();
+const props = defineProps<{ message: UserMessage }>();
+const { date, text, sender } = props.message;
+
+const user = useUserStore();
+
+const sendByUser = computed(() => user.accname === sender.accname)
 </script>
-
-<style scoped>
-.hyphens {
-  overflow-wrap: break-word;
-  hyphens: manual;
-}
-</style>
