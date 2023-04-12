15536ded21a87fdbdad762d449472b276847b858
GeForsen- <semen.neb@gmail.com>
2023-04-12 21:27:39 +0700

    LobbyCreationRadioInputGroup: add <label> support

--- src/module/create-lobby/lobby-creation-radio-input-group.vue
+++ src/module/create-lobby/lobby-creation-radio-input-group.vue
@@ -1,31 +1,40 @@
 <template>
   <div :id="groupId" class="my-4 flex gap-3 items-center justify-between">
-    <slot name="header"></slot>
+    <label :for="$attrs.name + '#' + selectedInputId">
+      <slot name="header"></slot>
+    </label>
     <div class="btn-group">
       <input
         v-for="value of inputValues"
         :key="value"
         v-bind="$attrs"
-        :checked="input === value" 
+        :checked="input === value"
         :data-title="value"
         :value="value"
         class="btn text-xl"
         type="radio"
         :id="$attrs.name + '#' + value"
-        @change="
-          emit('update:input', ($event.target as HTMLInputElement).value)
+        @change="$event => {
+          selectedInputId = ($event.target as HTMLInputElement).value;
+          emit('update:input', ($event.target as HTMLInputElement).value);
+        }
         "
       />
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from "vue";
+
 const emit = defineEmits<{
-  (e: "update:input", value: unknown): void
+  (e: "update:input", value: unknown): void;
 }>();
-const { inputValues, groupId } = defineProps<{
+
+const { inputValues, groupId, input } = defineProps<{
   inputValues: number[];
   groupId: string;
   input: unknown;
 }>();
+
+const selectedInputId = ref(input);
 </script>
