4d58f422270178297447503bdfc1a7e177963820
GeForsen- <semen.neb@gmail.com>
2023-04-12 20:42:12 +0700

    LobbyCreationRadioInputGroup: init

---
+++ src/module/create-lobby/lobby-creation-radio-input-group.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :id="groupId" class="my-4 flex gap-3 items-center justify-between">
+    <slot name="header"></slot>
+    <div class="btn-group">
+      <input
+        v-for="value of inputValues"
+        :key="value"
+        v-bind="$attrs"
+        :checked="input === value" 
+        :data-title="value"
+        :value="value"
+        class="btn text-xl"
+        type="radio"
+        :id="$attrs.name + '#' + value"
+        @change="
+          emit('update:input', ($event.target as HTMLInputElement).value)
+        "
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: "update:input", value: unknown): void
+}>();
+const { inputValues, groupId } = defineProps<{
+  inputValues: number[];
+  groupId: string;
+  input: unknown;
+}>();
+</script>
