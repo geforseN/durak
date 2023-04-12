9da9ca664f78a31e30b15867b255c0b1c9bf868c
GeForsen- <semen.neb@gmail.com>
2023-04-12 18:34:37 +0700

    LobbyCreationPopup: update <script> & add hook
    add hook (composable): useLobbySettings

--- src/module/create-lobby/lobby-creation-popup.vue
+++ src/module/create-lobby/lobby-creation-popup.vue
@@ -1,67 +1,55 @@
 <template>
-  <form class="border border-black p-4 mx-2 flex flex-col">
-    <h2 class="text-4xl">Параметры комнаты</h2>
-    <!--    -->
-    <div id="radio-player-count" class="my-4 flex gap-3 w-full items-center">
-      <h3>Количество игроков:</h3>
-      <div class="flex justify-between gap-x-4">
-        <div
-          v-for="maxUserCount of allowedMaxUserCount"
-          :key="maxUserCount"
-          class="relative w-8 h-8 flex flex-col items-center justify-center rounded-sm border-2 border-black"
-          :class="maxUserCount === lobbySettings.maxUserCount && 'border-2 border-orange-500'"
-        >
-          <input
-            class="w-0 h-0 focus:outline focus:outline-2 focus:outline-cyan-400 focus:outline-offset-[18px]"
-            v-model="lobbySettings.maxUserCount"
-            type="radio"
-            name="max-user-count"
-            :value="maxUserCount"
-            :id="`max-user-count#${maxUserCount}`"
-          />
-          <label
-            :for="`max-user-count#${maxUserCount}`"
-            class="absolute w-full select-none text-3xl flex justify-center items-center"
-          >
-            {{ maxUserCount }}
-          </label>
-        </div>
-      </div>
-    </div>
-    <!--    -->
-    <div id="radio-player-count" class="my-4 flex gap-3 w-full items-center justify-between">
+  <form
+    class="p-4 flex flex-col border-[4px] border-primary bg-secondary text-2xl"
+  >
+    <h2 class="text-4xl font-semibold">Параметры комнаты</h2>
+    <lobby-creation-popup-option id="radio-player-count">
+      <template #header><h3>Количество игроков:</h3></template>
+      <template #input>
+        <input
+          v-for="value of allowedMaxUserCount"
+          :key="value"
+          :value="value"
+          :data-title="value"
+          class="btn text-xl"
+          v-model="lobbySettings.maxUserCount"
+          type="radio"
+          name="max-user-count"
+          :id="'max-user-count#' + value"
+        />
+      </template>
+    </lobby-creation-popup-option>
+    <div
+      id="radio-player-count"
+      class="my-4 flex gap-3 items-center justify-between"
+    >
       <h3>Количество карт:</h3>
-      <div class="flex justify-between gap-x-4">
-        <template v-for="cardCount of allowedCardCount">
-          <div
-            v-if="canMakeFirstDistribution(cardCount)"
-            :key="cardCount"
-            class="relative w-8 h-8 flex flex-col items-center justify-center rounded-sm border-2 border-black"
-            :class="cardCount === lobbySettings.cardCount && 'border-2 border-orange-500'"
-          >
-            <input
-              class="w-0 h-0 focus:outline focus:outline-2 focus:outline-cyan-400 focus:outline-offset-[18px]"
-              v-model="lobbySettings.cardCount"
-              type="radio"
-              name="card-count"
-              :value="cardCount"
-              :id="`card-count#${cardCount}`"
-            />
-            <label
-              :for="`card-count#${cardCount}`"
-              class="absolute w-full select-none text-2xl flex justify-center items-center"
-            >
-              {{ cardCount }}
-            </label>
-          </div>
-        </template>
+      <div class="btn-group">
+        <input
+          v-for="cardCount of properCardCountValues"
+          :key="cardCount"
+          class="btn text-xl"
+          v-model="lobbySettings.cardCount"
+          :data-title="cardCount"
+          type="radio"
+          name="card-count"
+          :value="cardCount"
+          :id="`card-count#${cardCount}`"
+        />
       </div>
     </div>
     <!--    -->
-    <div id="select-game-type" class="my-4 flex gap-3 w-full items-center justify-between">
+    <div
+      id="select-game-type"
+      class="my-4 flex gap-3 w-full items-center justify-between"
+    >
       <h3><label for="game-types">Тип игры</label></h3>
       <div>
-        <select v-model="lobbySettings.gameType" id="game-types">
+        <select
+          class="select leading-8 select-bordered select-lg w-full max-w-xs"
+          v-model="lobbySettings.gameType"
+          id="game-types"
+        >
           <option v-for="gameType of allowedGameTypes" :value="gameType">
             {{ gameTypesDictionary[gameType] || gameType }}
           </option>
@@ -69,58 +57,42 @@
       </div>
     </div>
     <button
-      class="w-full bg-green-600 p-2 border-2 border-black text-2xl font-bold active:scale-95 focus:bg-green-700 transition"
+      class="mt-4 btn btn-lg bg-success border-info border-2 text-3xl font-bold hover:bg-success hover:border-info hover:hover:saturate-[1.3]"
       @click.prevent="createLobby()"
     >
       Создать комнату
     </button>
-    <button type="reset" @click="resetSettings()">Сброс</button>
+    <button
+      class="my-1 text-base"
+      type="reset"
+      @click.prevent="resetSettings()"
+    >
+      Сброс параметров
+    </button>
   </form>
 </template>
 <script setup lang="ts">
-import { reactive, watch } from "vue";
 import { gameLobbies } from "@/socket";
-import gameTypesDictionary from "@/utils/game-types-dictionary";
-import type {
-  MaxUserCount,
-  CardCount,
-  GameType,
-  LobbySettings,
-} from "@/module/game-lobbies/types";
+import LobbyCreationPopupOption from "./lobby-creation-popup-option.vue";
+import gameTypesDictionary from "@/utils/dictionary/game-types.dictionary";
+import useLobbySettings, {
+  allowedGameTypes,
+  allowedMaxUserCount,
+} from "./useLobbySettings";
 
-const allowedMaxUserCount: MaxUserCount[] = [2, 3, 4, 5, 6];
-const allowedCardCount: CardCount[] = [24, 36, 52];
-const allowedGameTypes: GameType[] = ["basic", "perevodnoy"];
+const emit = defineEmits<{
+  (e: "lobbyCreated"): void;
+}>();
 
-const defaultMaxUserCount = 2;
-const defaultCardCount = 36;
-const defaultGameType = "basic";
-
-const lobbySettings = reactive<LobbySettings>({
-  maxUserCount: defaultMaxUserCount,
-  gameType: defaultGameType,
-  cardCount: defaultCardCount,
-});
-
-const resetSettings = () => {
-  lobbySettings.maxUserCount = defaultMaxUserCount;
-  lobbySettings.gameType = defaultGameType;
-  lobbySettings.cardCount = defaultCardCount;
-};
-
-const canMakeFirstDistribution = (cardCount: number) => cardCount - lobbySettings.maxUserCount * 6 >= 0;
-
-watch(lobbySettings, (newLS, oldLS) => {
-  if (!canMakeFirstDistribution(oldLS.cardCount)) {
-    const index = allowedCardCount.findIndex((count) => count === oldLS.cardCount);
-    newLS.cardCount = allowedCardCount[index + 1];
-  }
-})
+const { lobbySettings, resetSettings, isProperCardCount, properCardCountValues } =
+  useLobbySettings();
 
 const createLobby = () => {
-  let { gameType, maxUserCount, cardCount } = lobbySettings;
-  if (!canMakeFirstDistribution(cardCount)) {}
-  console.log({ gameType, maxUserCount, cardCount });
-  gameLobbies.emit("createLobby", { gameType, maxUserCount, cardCount });
+  if (!isProperCardCount(lobbySettings.cardCount)) {
+    // TODO SHOW USER REASON WHY LOBBY CAN NOT BE CREATED
+    return;
+  }
+  emit("lobbyCreated");
+  gameLobbies.emit("createLobby", lobbySettings);
 };
 </script>
