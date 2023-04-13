ad59f91aa884062d8fb658db5099c8f4309addec
GeForsen- <semen.neb@gmail.com>
2023-04-13 16:13:15 +0700

    CreateLobby: add wrapper & styles

--- src/module/create-lobby/create-lobby.vue
+++ src/module/create-lobby/create-lobby.vue
@@ -1,6 +1,14 @@
 <template>
-  <lobby-creation-buttons :isCreatingLobby="isCreatingLobby" @toggle="toggle" />
-  <lobby-creation-popup v-show="isCreatingLobby" />
+  <div class="flex flex-col gap-8 items-stretch">
+    <lobby-creation-buttons
+      :isCreatingLobby="isCreatingLobby"
+      @toggle="toggle"
+    />
+    <lobby-creation-popup
+      v-show="isCreatingLobby"
+      @lobby-created="handleLobbyCreated"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,5 +17,9 @@ import LobbyCreationPopup from "@/module/create-lobby/lobby-creation-popup.vue";
 import { ref } from "vue";
 
 const isCreatingLobby = ref(false);
+
+const handleLobbyCreated = () => {
+  isCreatingLobby.value = false;
+};
 const toggle = () => (isCreatingLobby.value = !isCreatingLobby.value);
 </script>
