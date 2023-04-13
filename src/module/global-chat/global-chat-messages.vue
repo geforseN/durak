1a72f4e17134f0cfbe8746a66c75c81099740625
GeForsen- <semen.neb@gmail.com>
2023-04-13 12:31:48 +0700

    GlobalChat: add auto scroll to last message

--- src/module/global-chat/global-chat-messages.vue
+++ src/module/global-chat/global-chat-messages.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex-1 overflow-y-scroll">
+  <section class="flex-1 overflow-y-scroll" ref="messagesWrapper">
     <global-chat-user-message v-for="message of messages" :message="message" />
   </section>
 </template>
@@ -12,16 +12,28 @@ import type { UserMessage } from "@/module/global-chat/types";
 import formatTime from "@/utils/intl/format-time";
 
 const messages = ref<UserMessage[]>([]);
+const messagesWrapper = ref<HTMLElement>()
+
+const scrollToLastElement = (element: HTMLElement) => queueMicrotask(
+  () => element.scrollTo({ behavior: "smooth", top: element.scrollHeight })
+);
 
 globalChat.on("sendMessage", (message: UserMessage) => {
   message.date = formatTime(message.date as number);
   messages.value.push(message);
+  if (!messagesWrapper.value) return;
+  scrollToLastElement(messagesWrapper.value);
 });
+
 
 globalChat.on("restoreHistory", (globalChatHistory: UserMessage[]) => {
   messages.value = globalChatHistory.map((message) => {
-    message.date = formatTime(message.date as number);
+    if (typeof message.date === "number") {
+      message.date = formatTime(message.date);
+    }
     return message;
   });
+  if (!messagesWrapper.value) return;
+  scrollToLastElement(messagesWrapper.value);
 });
 </script>
