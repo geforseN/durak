5e381eb2f7e812a924bb8e64a92d1f91c567987f
GeForsen- <semen.neb@gmail.com>
2023-04-18 15:09:17 +0700

    useSelfStore: lint & add throw

--- src/stores/game/self.store.ts
+++ src/stores/game/self.store.ts
@@ -1,6 +1,11 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import type { PlayerRole, Self, UserInfo, Card } from "@/module/card-game/types";
+import type {
+  PlayerRole,
+  Self,
+  UserInfo,
+  Card,
+} from "@/module/card-game/types";
 
 const defaultUserInfo: UserInfo = {
   accname: "",
@@ -21,13 +26,15 @@ export const useGameSelfStore = defineStore("gameSelf", () => {
     const cardIndex = self.value.cards.findIndex(
       (card) => card.suit === suit && card.rank === rank,
     );
-    if (!cardIndex) return alert("Can not remove card from Self");
+    if (!cardIndex) throw new Error("Can not remove card from Self");
     self.value.cards.splice(cardIndex, 1);
   };
 
   const changeRole = (role: PlayerRole, accname: string) => {
-    if (accname === self.value.info.accname) self.value.role = role;
-    throw new Error("Self has not found");
+    if (accname !== self.value.info.accname) {
+      throw new Error("Self has not found");
+    }
+    self.value.role = role;
   };
 
   return { pushCard, removeCard, changeRole, self };
