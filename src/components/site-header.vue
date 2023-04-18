b8707a800970049d87778ef31b92727329f23c81
GeForsen- <semen.neb@gmail.com>
2023-04-18 22:28:06 +0700

    SiteHeader: update styles

--- src/components/site-header.vue
+++ src/components/site-header.vue
@@ -1,23 +1,37 @@
 <template>
   <header>
-    <ul class="flex items-center gap-2 px-4 py-1 bg-emerald-200">
-      <li>
-        <router-link to="/" class="underline">Главная</router-link>
-      </li>
-      <li class="ml-auto" v-if="username">
-        <router-link :to="`/profile/${personalLink}`" class="inline-block border border-black h-10 w-10">
-          <img :src="photoUrl" :alt="`${username} profile picture`" :title="username">
-        </router-link>
-      </li>
-      <template v-if="!username">
-        <li class="ml-auto">
-          <router-link to="/auth/registration" class="underline">Зарегистрироваться</router-link>
-        </li>
+    <nav>
+      <ul class="navbar bg-neutral-700">
         <li>
-          <router-link to="/auth/login" class="underline">Войти</router-link>
+          <router-link
+            to="/" class="btn btn-ghost text-xl text-secondary"
+            active-class="underline underline-offset-8 text-accent decoration-black">Главная
+          </router-link>
         </li>
-      </template>
-    </ul>
+        <template v-if="username">
+          <li class="ml-auto">
+            <router-link
+              :to="`/profile/${personalLink}`" class="border border-black w-10 avatar aspect-square">
+              <img :src="photoUrl" :alt="`${username} profile picture`" :title="username" />
+            </router-link>
+          </li>
+        </template>
+        <template v-else>
+          <li class="ml-auto">
+            <router-link
+              to="/auth/registration" class="btn btn-ghost text-xl text-neutral-content">
+              Зарегистрироваться
+            </router-link>
+          </li>
+          <li class="ml-4">
+            <router-link
+              to="/auth/login" class="btn btn-ghost text-xl text-neutral-content">
+              Войти
+            </router-link>
+          </li>
+        </template>
+      </ul>
+    </nav>
   </header>
 </template>
 <script setup lang="ts">
