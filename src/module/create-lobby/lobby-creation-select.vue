d46612dbcd1f1cca8b4b9d7cb938ff5bdcaf8972
GeForsen- <semen.neb@gmail.com>
2023-04-19 16:15:26 +0700

    LobbyCreationSelect: init

---
+++ src/module/create-lobby/lobby-creation-select.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-4 flex flex-wrap gap-0.5 max-[320px]:flex-col justify-between items-baseline">
+    <label :for="id">
+      <slot></slot>
+    </label>
+    <select
+      class="max-w-xs select select-sm text-xl min-[420px]:select-md min-[420px]:text-2xl
+      bg-neutral select-bordered text-secondary text-center focus:outline-primary focus:outline-2"
+      v-bind="$attrs" :id="id">
+      <option v-for="option of options" :value="option" :name="option">
+        {{ optionsDictionary[option] || option }}
+      </option>
+    </select>
+  </div>
+</template>
+<script setup lang="ts">
+const { options, optionsDictionary, id } =
+  defineProps<{ options: unknown[], optionsDictionary: Record<string, string>, id: string }>();
+</script>
