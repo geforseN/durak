ee80e90aeef67180e1a9191856177a81f2af1c4c
GeForsen- <semen.neb@gmail.com>
2023-04-19 16:46:33 +0700

    GameLobbySlots: add joinLobby on click

--- src/module/game-lobbies/game-lobby-slots.vue
+++ src/module/game-lobbies/game-lobby-slots.vue
@@ -10,10 +10,12 @@
       <button
         v-else
         class="sm:w-[86px] sm:h-[137px] md:h-[172px] md:w-[108px] lg:h-[207px] lg:w-[130px]
-         flex justify-center items-center
-         p-0 vertical-squares-pattern hover:scale-[1.01] hover:border-primary hover:bg-success border-4 border-primary rounded-lg">
-        <span
-          class="text-xs md:text-base lg:text-lg font-bold bg-white w-3/4 border-2 border-primary rounded text-black py-2 px-1">
+        flex justify-center items-center
+        p-0 vertical-squares-pattern hover:scale-[1.01] hover:border-primary hover:bg-success border-4 border-primary rounded-lg"
+        @click="joinLobby(index)">
+      <span
+        class="text-xs md:text-base lg:text-lg font-bold bg-white w-3/4
+         sm:border-2 sm:border-primary rounded text-black py-2 px-1">
           Войти в лобби
         </span>
       </button>
@@ -25,6 +27,7 @@
 import { computed } from "vue";
 import type { Lobby } from "@/module/game-lobbies/types";
 import GameLobbyUser from "@/module/game-lobbies/game-lobby-user.vue";
+import { gameLobbies } from "@/socket";
 
 const { lobby } = defineProps<{ lobby: Lobby }>();
 
@@ -32,6 +35,10 @@ const lobbyUsers = computed(() => {
   const length = lobby.settings.maxUserCount;
   return [...Array(length)].map((_, i) => lobby.users[i]);
 });
+
+const joinLobby = (cellIndex: number = -1) => {
+  gameLobbies.emit("joinLobby", lobby.id, cellIndex);
+};
 </script>
 <style scoped>
 .vertical-squares-pattern {
