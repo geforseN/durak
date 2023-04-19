fd1f3859de57663e06e62693ffd8000667a41f73
GeForsen- <semen.neb@gmail.com>
2023-04-19 16:22:59 +0700

    LobbyCreationButtons: add styles

--- src/module/create-lobby/lobby-creation-buttons.vue
+++ src/module/create-lobby/lobby-creation-buttons.vue
@@ -1,17 +1,16 @@
 <template>
   <button
     v-show="!isCreatingLobby"
-    class="text-2xl text-gray-900 border-2 border-primary btn btn-lg bg-success shadow-lg shadow-success/90 hover:bg-success hover:saturate-[1.3]"
-    @click="$emit('toggle')"
-  >
+    class="h-auto max-w-md text-xl text-gray-900 border-2 border-primary
+    btn btn-lg bg-success shadow-lg shadow-success/90 hover:bg-success hover:saturate-[1.3]"
+    @click="$emit('toggle')">
     Начать создание комнаты
   </button>
-
   <button
     v-show="isCreatingLobby"
-    class="text-2xl text-gray-900 border-2 border-primary btn btn-lg bg-error shadow-lg shadow-error/90 hover:bg-error hover:saturate-[0.8]"
-    @click="$emit('toggle')"
-  >
+    class="h-auto max-w-md text-xl text-gray-900 border-2 border-primary
+    btn btn-lg bg-error shadow-lg shadow-error/90 hover:bg-error hover:saturate-[0.8]"
+    @click="$emit('toggle')">
     Отменить создание комнаты
   </button>
 </template>
