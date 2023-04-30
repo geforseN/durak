3ea05482588d52d62ddff771a8c41f36cd2a1633
GeForsen- <semen.neb@gmail.com>
2023-04-30 16:05:46 +0700

    EnemyProfileRoleBadge: init

---
+++ src/module/card-game/EnemyProfileRoleBadge.vue
@@ -0,0 +1,13 @@
+<template>
+  <shield-svg v-if="role === 'Defender'" />
+  <swords-svg v-else-if="role === 'Attacker'" />
+</template>
+
+<script setup lang="ts">
+import type { Enemy } from "@/module/card-game/types";
+import ShieldSvg from "@/components/svg/Shield.vue";
+import SwordsSvg from "@/components/svg/Swords.vue";
+
+const { enemy } = defineProps<{ enemy: Enemy }>();
+const { role } = enemy;
+</script>
