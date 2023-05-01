48b9e04d58d2d644865d84bfa81f27927f21818e
GeForsen- <semen.neb@gmail.com>
2023-05-01 17:32:28 +0700

    Card: make power optional

--- src/module/card-game/types/Card.ts
+++ src/module/card-game/types/Card.ts
@@ -1,7 +1,7 @@
 export type Card = {
   rank: Rank;
   suit: Suit;
-  power: number;
+  power?: number;
 }
 
 export type Rank = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
