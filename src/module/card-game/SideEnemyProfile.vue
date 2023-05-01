227d95346698c2367d1b3acfcdf80442d3c6c52b
GeForsen- <semen.neb@gmail.com>
2023-05-01 17:46:12 +0700

    SideEnemyPlayer: init

---
+++ src/module/card-game/SideEnemyProfile.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="flex rounded flex-col gap-x-2 items-center bg-primary overflow-hidden">
+    <avatar :enemy="enemy" :is-allowed-to-move="isAllowedToMove" />
+    <role-badge :enemy="enemy" />
+    <card-number :enemy="enemy" />
+    <router-link
+      :to="`/profile/${info.personalLink}`"
+      target="_blank"
+      :title="info.nickname"
+      class="link link-hover">
+      <span class="font-bold line-clamp-3 break-all -------------------> w-8 <----------------------------">
+        {{ info.nickname + info.nickname + info.nickname }}
+      </span>
+    </router-link>
+  </section>
+</template>
+
+<script setup lang="ts">
+import type { Enemy } from "@/module/card-game/types";
+import Avatar from "@/module/card-game/EnemyProfileAvatar.vue";
+import CardNumber from "@/module/card-game/EnemyProfileCardNumber.vue";
+import RoleBadge from "@/module/card-game/EnemyProfileRoleBadge.vue";
+
+const { enemy, isAllowedToMove } = defineProps<{
+  enemy: Enemy,
+  isAllowedToMove: boolean
+}>();
+const { info } = enemy;
+
+</script>
