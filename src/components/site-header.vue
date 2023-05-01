5e63251e04bca920ddd0ce8c247f9187dc04c086
GeForsen- <semen.neb@gmail.com>
2023-05-01 17:33:42 +0700

    Header: lint & add Home SVG

--- src/components/site-header.vue
+++ src/components/site-header.vue
@@ -3,29 +3,27 @@
     <nav>
       <ul class="navbar bg-neutral-700">
         <li>
-          <router-link
-            to="/" class="btn btn-ghost text-xl text-secondary"
-            active-class="underline underline-offset-8 text-accent decoration-black">Главная
+          <router-link to="/" class="btn btn-ghost text-xl text-secondary"
+            active-class="underline underline-offset-8 text-accent decoration-black">
+            <home class="mr-2" />
+            Главная
           </router-link>
         </li>
         <template v-if="username">
           <li class="ml-auto">
-            <router-link
-              :to="`/profile/${personalLink}`" class="border border-black w-10 avatar aspect-square">
+            <router-link :to="`/profile/${personalLink}`" class="border border-black w-10 avatar aspect-square">
               <img :src="photoUrl" :alt="`${username} profile picture`" :title="username" />
             </router-link>
           </li>
         </template>
         <template v-else>
           <li class="ml-auto">
-            <router-link
-              to="/auth/registration" class="btn btn-ghost text-xl text-neutral-content">
+            <router-link to="/auth/registration" class="btn btn-ghost text-xl text-neutral-content">
               Зарегистрироваться
             </router-link>
           </li>
           <li class="ml-4">
-            <router-link
-              to="/auth/login" class="btn btn-ghost text-xl text-neutral-content">
+            <router-link to="/auth/login" class="btn btn-ghost text-xl text-neutral-content">
               Войти
             </router-link>
           </li>
@@ -37,6 +35,7 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user.store";
 import { storeToRefs } from "pinia";
+import Home from "@/components/svg/Home.vue";
 
 const { photoUrl, username, personalLink } = storeToRefs(useUserStore());
 </script>
