5a17b6e4a990b825d9a7868eadae43123febe8be
GeForsen- <semen.neb@gmail.com>
2023-05-01 17:38:53 +0700

    useGameEnemiesStore: add computed for side enemies

--- src/stores/game/enemies.store.ts
+++ src/stores/game/enemies.store.ts
@@ -1,16 +1,46 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 import type { Enemy } from "@/module/card-game/types";
+
+type EnemiesSideIndexes = {
+  top: number[]
+  left?: number[]
+  right?: number[]
+}
+
+const enemiesSides: Record<number, EnemiesSideIndexes> = {
+  1: {
+    top: [0]
+  },
+  2: {
+    top: [0, 1]
+  },
+  3: {
+    left: [0],
+    top: [1],
+    right: [2]
+  },
+  4: {
+    left: [0],
+    top: [1, 2],
+    right: [3]
+  },
+  5: {
+    left: [0, 1],
+    top: [2],
+    right: [3, 4]
+  }
+}
 
 export const useGameEnemiesStore = defineStore("gameEnemies", () => {
   const enemies = ref<Enemy[]>([]);
 
   const findBy = ({ accname }: { accname: string }) => {
-    return enemies.value.find((enemy) => enemy.info.accname === accname);
+    return enemies.value.find((enemy) => enemy.id === accname);
   };
 
   const has = ({ accname }: { accname: string }) => {
-    return enemies.value.some((enemy) => enemy.info.accname === accname);
+    return enemies.value.some((enemy) => enemy.id === accname);
   };
 
   const changeEnemyCardCount = (accname: string, cardCount: number) => {
@@ -19,5 +49,22 @@ export const useGameEnemiesStore = defineStore("gameEnemies", () => {
     enemy.cardCount = cardCount;
   };
 
-  return { enemies, findBy, changeEnemyCardCount, has };
+  const __findSideEnemies__ = (side: keyof EnemiesSideIndexes) => {
+    const allowedIndexes = enemiesSides[enemies.value.length][side];
+    return enemies.value.filter((_, index) => {
+      return allowedIndexes?.includes(index);
+    })
+  }
+
+  const leftEnemies = computed(() => {
+    return __findSideEnemies__("left")
+  })
+  const topEnemies = computed(() => {
+    return __findSideEnemies__("top")
+  })
+  const rightEnemies = computed(() => {
+    return __findSideEnemies__("right")
+  })
+
+  return { enemies, findBy, changeEnemyCardCount, has, leftEnemies, topEnemies, rightEnemies };
 });
