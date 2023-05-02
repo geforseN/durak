ab5242b4d4a53009ba8e811779734e591ef23623
GeForsen- <semen.neb@gmail.com>
2023-05-02 20:40:25 +0700

    EnemyProfile: update

--- src/module/card-game/EnemyProfile.vue
+++ src/module/card-game/EnemyProfile.vue
@@ -1,29 +1,26 @@
 <template>
-  <section
-    class="sm:max-w-max flex items-center gap-x-2 rounded-xl bg-purple-300 sm:p-2 border-black border sm:border-2">
+  <section class="flex items-center gap-x-2 rounded-xl bg-purple-300 border border-black 
+  sm:border-2 sm:p-2">
     <avatar :enemy="enemy" :is-allowed-to-move="isAllowedToMove" />
-    <div class="flex flex-col items-start overflow-hidden ">
+    <div class="flex flex-col items-start">
       <div class="flex items-center justify-between">
-        <router-link
-          :to="`/profile/${info.personalLink}`"
-          target="_blank"
-          :title="info.nickname"
-          class="link link-hover">
+        <router-link :to="`/profile/${info.personalLink}`" target="_blank" :title="info.nickname" class="link link-hover">
           <span class="font-bold line-clamp-3 break-all">
             {{ info.nickname + info.nickname + info.nickname }}
           </span>
         </router-link>
-        <role-badge :enemy="enemy" />
+        <role-badge :role="enemy.role" />
       </div>
-      <card-number :enemy="enemy" />
+      <div class="flex">
+        <stacked-cards :count="enemy.cardCount" />
+      </div>
     </div>
   </section>
-  
 </template>
 <script setup lang="ts">
 import type { Enemy } from "@/module/card-game/types";
 import Avatar from "@/module/card-game/EnemyProfileAvatar.vue";
-import CardNumber from "@/module/card-game/EnemyProfileCardNumber.vue";
+import StackedCards from "@/module/card-game/EnemyProfileStackedCards.vue";
 import RoleBadge from "@/module/card-game/EnemyProfileRoleBadge.vue";
 
 const { enemy, isAllowedToMove } = defineProps<{
