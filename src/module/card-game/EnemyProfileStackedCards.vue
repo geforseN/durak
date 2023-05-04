99b5979b743e35793be1325d53961fdb8228242e
GeForsen- <semen.neb@gmail.com>
2023-05-04 18:52:53 +0700

    EnemyProfileStackCards: update class name

--- src/module/card-game/EnemyProfileStackedCards.vue
+++ src/module/card-game/EnemyProfileStackedCards.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-for="number of props.count" :data-number="number" :style="{ '--x': 2.7 }"
-    class="relative h-[28px] w-[21px] sm:h-[39px] sm:w-[27px] md:h-[55px] md:w-[38px] grid place-items-end -mx-2 md:-mx-3.5 vertical-squares-pattern bg-indigo-600 border border-gray-300 hover:z-10 hover:-translate-y-0.5">
+    class="relative h-[28px] w-[21px] sm:h-[39px] sm:w-[27px] md:h-[55px] md:w-[38px] grid place-items-end -mx-2 md:-mx-3.5 card-bg bg-indigo-600 border border-gray-300 hover:z-10 hover:-translate-y-0.5">
   </div>
 </template>
 <script setup lang="ts">
