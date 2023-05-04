788884369b025e395852608d4ed2cb736805afa1
GeForsen- <semen.neb@gmail.com>
2023-05-04 17:59:38 +0700

    EnemyProfileRoleBage: add styles

--- src/module/card-game/EnemyProfileRoleBadge.vue
+++ src/module/card-game/EnemyProfileRoleBadge.vue
@@ -1,13 +1,11 @@
 <template>
-  <shield-svg v-if="role === 'Defender'" />
-  <swords-svg v-else-if="role === 'Attacker'" />
+  <shield-svg class="z-10 border border-black rounded bg-blue-400" v-if="props.role === 'Defender'" />
+  <swords-svg class="z-10 border border-black rounded bg-red-400" v-else-if="props.role === 'Attacker'" />
 </template>
 
 <script setup lang="ts">
-import type { Enemy } from "@/module/card-game/types";
+import type { PlayerRole } from "./types";
 import ShieldSvg from "@/components/svg/Shield.vue";
 import SwordsSvg from "@/components/svg/Swords.vue";
-
-const { enemy } = defineProps<{ enemy: Enemy }>();
-const { role } = enemy;
+const props = defineProps<{ role: PlayerRole }>();
 </script>
