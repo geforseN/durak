63c196043a1fb0f3f0371a32ccab8d63276a97f0
GeForsen- <semen.neb@gmail.com>
2023-05-04 17:36:22 +0700

    add xxs & xs media screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'xxs': '310px',
+      'xs': '475px', /* 
+      'sm': '640px'
+      'md': '768px'
+      'lg': '1024px'
+      'xl': '1280px'
+      '2xl': '1536px' */
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [
