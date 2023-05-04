c1f7ccf0bba282d399d46dca79eaaa08fd8998ef
GeForsen- <semen.neb@gmail.com>
2023-05-04 18:52:18 +0700

    init index file for EnemyProfiles

---
+++ src/module/card-game/enemy-profile/index.ts
@@ -0,0 +1,4 @@
+export {default as Left} from "./EnemyProfileLeft.vue";
+export {default as Right} from "./EnemyProfileRight.vue";
+export {default as TopRight} from "./EnemyProfileTopRight.vue";
+export {default as TopLeft} from "./EnemyProfileTopLeft.vue";
