7ea8d6f0581285b57b3688f389a24dc6c155bd64
GeForsen- <semen.neb@gmail.com>
2023-05-04 17:42:14 +0700

    Desk: change styles

--- src/module/card-game/Desk.vue
+++ src/module/card-game/Desk.vue
@@ -1,20 +1,17 @@
 <template>
-  <div class="m-auto items-center justify-items-center w-max grid grid-cols-3 grid-flow-row">
-    <div v-for="(slot, index) of deskSlots"
-         class="relative bg-orange-400 border h-[160px] w-[114px] flex justify-center items-center font-bold text-3xl"
-         @drop="emit('dropCardOnDesk', $event, slot, index)"
-         @dragenter.prevent
-         @dragover.prevent
-    >
-      <div v-if="!slot.attackCard"
-           class="bg-indigo-400 h-[116px] w-[83px] flex justify-center items-center">
-        {{ index }}
+  <div class="max-w-5xl grid grid-cols-2 xs:grid-cols-3 grid-flow-row bg-primary justify-items-center">
+    <div v-for="(slot, index) of deskSlots" :key="index" 
+      class="relative flex justify-center items-center h-[83px] w-[60px] xxs:h-[116px] xxs:w-[83px] sm:h-[162px] sm:w-[116px] 
+        font-bold text-3xl border sm:border-4 border-primary rounded" 
+      @drop="emit('dropCardOnDesk', $event, slot, index)"
+      @dragenter.prevent @dragover.prevent>
+      <div v-if="!slot.attackCard" class="w-full h-full bg-info flex justify-center items-center">
+        <span class="opacity-50">{{ index }}</span>
       </div>
       <template v-else>
-        <game-card :suit="slot.attackCard.suit" :rank="slot.attackCard.rank"/>
+        <game-card class="w-full h-full" :suit="slot.attackCard.suit" :rank="slot.attackCard.rank" />
         <game-card v-if="slot.defendCard" :suit="slot.defendCard.suit" :rank="slot.defendCard.rank"
-                   class="absolute inset-1/2 -translate-y-[42%] -translate-x-[35%] z-10"
-        />
+          class="w-full h-full absolute inset-1/2 -translate-y-[42%] -translate-x-[35%] z-10" />
       </template>
     </div>
   </div>
