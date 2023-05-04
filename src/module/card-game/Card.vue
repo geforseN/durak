9c2eb3c3ef183c45a4f85bb97ec4fc88fc8c899b
GeForsen- <semen.neb@gmail.com>
2023-05-04 17:41:40 +0700

    Card: fix emit data & add computed

--- src/module/card-game/Card.vue
+++ src/module/card-game/Card.vue
@@ -1,23 +1,23 @@
 <template>
   <div
-    :style="{'backgroundImage':`url(https://deckofcardsapi.com/static/img/${rank.at(-1)}${suitsDictionary[suit]}.png)`}"
-    draggable="true"
-    @dragstart="emit('cardDrag', $event, card)"
-    @dragend.prevent="emit('cardDragEnd', $event, card)"
-    class="border-2 border-black h-[116px] w-[83px] rounded bg-cover bg-black"
-  />
+    :style="{ 'backgroundImage': `url(https://deckofcardsapi.com/static/img/${cardId}.png)` }"
+    draggable="true" @dragstart="emit('cardDrag', $event, { rank, suit })"
+    @dragend.prevent="emit('cardDragEnd', $event, { rank, suit })"
+    class="h-[116px] w-[83px] border-2 border-black rounded bg-cover bg-black"></div>
 </template>
 
 <script setup lang="ts">
 import type { Rank, Suit } from "@/module/card-game/types";
 import suitsDictionary from "@/utils/dictionary/suits.dictionary";
+import { computed } from "vue";
 
 export type Card = {
   rank: Rank,
   suit: Suit,
   power?: number
 }
-const { rank, suit } = defineProps<Card>();
+const props = defineProps<Card>();
+const cardId = computed(() => `${props.rank.at(-1)}${suitsDictionary[props.suit]}`)
 
 const emit = defineEmits<{
   (e: "cardDrag", $event: Event, card: Card): void
