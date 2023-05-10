fd9d23087f39bfd4b34cbbe9a767918f248deb5c
GeForsen- <semen.neb@gmail.com>
2023-05-10 21:56:20 +0700

    durakGame: add composable usage

--- src/views/durak-game.vue
+++ src/views/durak-game.vue
@@ -6,7 +6,7 @@
         <component :is="index % 2 ? EnemyProfile.TopRight : EnemyProfile.TopLeft" :enemy="enemy"
           :is-allowed-to-move="enemy.id === gameStateStore.allowedPlayerId" />
       </template>
-      </div>
+    </div>
     <div class="w-full flex justify-evenly gap-x-0 xxs:gap-x-2 xs:gap-x-4 xs:px-2">
       <div class="flex flex-col justify-evenly gap-y-4">
         <EnemyProfile.Left v-for="enemy of enemiesStore.leftEnemies" :key="enemy.id" :enemy="enemy"
@@ -26,7 +26,7 @@
             <basic-card v-if="gameStateStore.gameState.trumpCard" class="absolute top-0 right-6 rotate-90"
               v-bind="gameStateStore.gameState.trumpCard" />
           </div>
-    </div>
+        </div>
         <game-desk :desk-slots="deskStore.deskSlots" @drop-card-on-desk="handleCardDropOnDesk" />
       </div>
       <div class="flex flex-col justify-evenly gap-y-4">
@@ -66,82 +66,12 @@ import {
   useGameSelfStore,
   useGameEnemiesStore,
 } from "@/stores/game";
-import { ref } from "vue";
-import { io } from "socket.io-client";
 
-const refOfBoard = ref<HTMLElement>();
-const draggedCard = ref<HTMLElement | null>(null);
-
-const route = useRoute();
-const gameSocket = io(
-  `${import.meta.env.VITE_SOCKET_SERVER_ADDRESS}/game/${route.params.gameId}`,
-  { withCredentials: true },
-);
-gameSocket.onAny((event: any, ...args: any) => {
-  console.log(`got event ${event}`);
-  console.log(...args);
-});
-
-const notificationStore = useNotificationStore();
 const enemiesStore = useGameEnemiesStore();
 const selfStore = useGameSelfStore();
 const deskStore = useGameDeskStore();
 const gameStateStore = useGameStateStore();
 
-const handleCardDropOnDesk = (event: any, slot: DeskSlot, slotIndex: number) => {
-  const card = JSON.parse(event.dataTransfer.getData("card"));
-  gameSocket.emit("superPlayer__putCardOnDesk", card, slotIndex);
-};
-
-const handleCardDrag = (event: any, card: Card) => {
-  console.log(event.target);
-  // console.log(event.currentTarget);
-  draggedCard.value = event.target;
-  console.log(card);
-  event.dataTransfer.setData("card", JSON.stringify(card));
-  console.log("__ASD__");
-};
-
-const handleCardDragEnd = (event: any, card: Card) => {
-  draggedCard.value = null;
-  console.log("__asd__");
-};
-
-const stopMove = () => gameSocket.emit("superPlayer__stopMove");
-
-;[
-  { eventName: "state__restore", listener: gameStateStore.restore },
-  { eventName: "notification__send", listener: notificationStore.addNotificationInQueue },
-  { eventName: "player__changeRole", listener: gameStateStore.changeRole },
-  // { eventName: "attackUI__setStatus", callback: UIStore.setAttackUI },
-  // { eventName: "defendUI__setStatus", callback: UIStore.setDefendUI },
-  { eventName: "self__removeCard", listener: selfStore.removeCard },
-  { eventName: "player__receiveCards", listener: selfStore.pushCard },
-  { eventName: "enemy__changeCardCount", listener: enemiesStore.changeEnemyCardCount },
-  { eventName: "desk__clear", listener: deskStore.clear },
-  { eventName: "player__insertCard", listener: deskStore.insertCard },
-].forEach(({ eventName, listener }) => gameSocket.on(eventName, listener));
-
-gameSocket.on("talon__showTrumpCard", gameStateStore.setTrumpCard); // TODO
-gameSocket.on("player__allowedToMove", (accname: string) => {
-  if (selfStore.selfId !== accname && !enemiesStore.has({ accname })) {
-    throw new Error("Can't change allowedPlayerId");
-  }
-  gameStateStore.gameState.allowedPlayerId = accname;
-});
-gameSocket.on("desk__pushToDiscard", () => {
-});
-gameSocket.on("talon__distributeCards", (accname: string, cardCount: number) => {
-  // console.log(refOfBoard.value?.getBoundingClientRect());
-  // if (draggedCard.value) {
-  //   console.warn(draggedCard.value);
-  // }
-});
-gameSocket.on("talon__moveTrumpCardTo", (accname: string) => {
-});
-gameSocket.on("defender__lostRound", (accname: string, roundNumber: number) => {
-});
-gameSocket.on("defender__wonRound", (accname: string, roundNumber: number) => {
-});
+const { handleCardDrag, handleCardDragEnd, handleCardDropOnDesk, stopMove } = useDurakGame({ debug: true });
 
 </script>
