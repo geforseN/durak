0b9dfea2ac390c56090dc509fcfb5f69e7538669
GeForsen- <semen.neb@gmail.com>
2023-05-10 21:54:33 +0700

    durakGame: update template

--- src/views/durak-game.vue
+++ src/views/durak-game.vue
@@ -1,25 +1,37 @@
 <template>
-  <main
-    class="min-h-[90vh] flex flex-col justify-evenly items-center
-  bg-emerald-400 xxs:bg-neutral-500 xs:bg-indigo-600 sm:bg-yellow-500 md:bg-green-500 lg:bg-red-500 xl:bg-purple-500 2xl:bg-amber-500">
-    <div class="w-full h-full bg-red-900 flex justify-around">
-      <enemy-profile class="flex-1" v-for="enemy of enemiesStore.topEnemies" :key="enemy.id" :enemy="enemy"
-        :isAllowedToMove="enemy.id === gameStateStore.allowedPlayerId" />
+  <main class="min-h-[90vh] flex flex-col justify-evenly bg-secondary gap-y-2">
+    <div
+      class="w-full h-full flex justify-around items-center self-center bg-neutral rounded max-lg:rounded-t-none max-w-5xl p-2 border-2 border-neutral-900">
+      <template v-for="(enemy, index) of enemiesStore.topEnemies">
+        <component :is="index % 2 ? EnemyProfile.TopRight : EnemyProfile.TopLeft" :enemy="enemy"
+          :is-allowed-to-move="enemy.id === gameStateStore.allowedPlayerId" />
+      </template>
       </div>
-    <div class="w-full flex justify-around">
-      <div>
-        <side-enemy-profile v-for="enemy of enemiesStore.leftEnemies" :key="enemy.id" 
-          :enemy="enemy" :isAllowedToMove="enemy.id === gameStateStore.allowedPlayerId" />
+    <div class="w-full flex justify-evenly gap-x-0 xxs:gap-x-2 xs:gap-x-4 xs:px-2">
+      <div class="flex flex-col justify-evenly gap-y-4">
+        <EnemyProfile.Left v-for="enemy of enemiesStore.leftEnemies" :key="enemy.id" :enemy="enemy"
+          :is-allowed-to-move="enemy.id === gameStateStore.allowedPlayerId" />
+      </div>
+      <div
+        class="w-full max-w-xs xs:max-w-md py-5 rounded-xl p-2 flex gap-y-3 flex-col items-center bg-neutral border-neutral-900 border-2">
+        <div class="self-end flex w-full justify-around">
+          <div :style="{ '--x': 2.7 }" v-if="!gameStateStore.gameState.isDiscardEmpty" class="relative left-3 card-bg border-2 border-black rounded bg-cover bg-indigo-600
+              h-[77px] w-[55px] xxs:h-[96px] xxs:w-[69px] md:h-[105px] md:w-[75px] lg:h-[127px] lg:w-[91px]">
+          </div>
+          <div class="relative">
+            <div :style="{ '--x': 2.7 }"
+              v-if="!gameStateStore.gameState.isTalonHasOneCard && !gameStateStore.gameState.isTalonEmpty"
+              class="relative z-10 card-bg border-2 border-black rounded bg-cover bg-indigo-600 h-[77px] w-[55px] xxs:h-[96px] xxs:w-[69px] md:h-[105px] md:w-[75px] lg:h-[127px] lg:w-[91px]">
+            </div>
+            <basic-card v-if="gameStateStore.gameState.trumpCard" class="absolute top-0 right-6 rotate-90"
+              v-bind="gameStateStore.gameState.trumpCard" />
+          </div>
     </div>
-      <div class="bg-secondary">
         <game-desk :desk-slots="deskStore.deskSlots" @drop-card-on-desk="handleCardDropOnDesk" />
-    <template v-if="gameStateStore.gameState.trumpCard">
-          <p> Trump Card: {{ gameStateStore.gameState.trumpCard?.rank + gameStateStore.gameState.trumpCard?.suit }}</p>
-    </template>
       </div>
-      <div>
-        <side-enemy-profile v-for="enemy of enemiesStore.rightEnemies" :key="enemy.id" 
-        :enemy="enemy" :isAllowedToMove="enemy.id === gameStateStore.allowedPlayerId"/>
+      <div class="flex flex-col justify-evenly gap-y-4">
+        <EnemyProfile.Right v-for="enemy of enemiesStore.rightEnemies" :key="enemy.id" :enemy="enemy"
+          :is-allowed-to-move="enemy.id === gameStateStore.allowedPlayerId" />
       </div>
     </div>
     <div class="flex flex-col items-center">
