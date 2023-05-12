2e62f43f45af342e43b7030d99e15f5d12ea097e
GeForsen- <semen.neb@gmail.com>
2023-05-12 18:49:03 +0700

    Card: add composable usage & dynamic classes & exported props type

--- src/module/card-game/Card.vue
+++ src/module/card-game/Card.vue
@@ -1,27 +1,23 @@
 <template>
-  <div
-    :style="{ 'backgroundImage': `url(https://deckofcardsapi.com/static/img/${cardId}.png)` }"
-    draggable="true" @dragstart="emit('cardDrag', $event, { rank, suit })"
-    @dragend.prevent="emit('cardDragEnd', $event, { rank, suit })"
-    class="h-[116px] w-[83px] border-2 border-black rounded bg-cover bg-black"></div>
+  <div :style="{ 'backgroundImage': `url(https://deckofcardsapi.com/static/img/${id}.png)` }" :class="[
+    canBeUsedForDefense && !isTrump && 'outline outline-blue-500',
+    canBeUsedForDefense && isTrump && 'outline outline-purple-500 outline-4',
+    canBeUsedForTransferMove && 'outline outline-red-500 rotate-2',
+    canBeUsedForAttack && 'outline outline-yellow-500',
+  ]" draggable="true" @dragstart="emit('cardDrag', $event, { rank, suit })"
+       @dragend.prevent="emit('cardDragEnd', $event, { rank, suit })"
+       class="h-[116px] w-[83px] border-2 border-black rounded bg-cover bg-black"></div>
 </template>
 
 <script setup lang="ts">
-import type { Rank, Suit } from "@/module/card-game/types";
-import suitsDictionary from "@/utils/dictionary/suits.dictionary";
-import { computed } from "vue";
+import type { Card } from "@/module/card-game/types";
+import { useGameCard } from "@/module/card-game/composable/useGameCard";
 
-export type Card = {
-  rank: Rank,
-  suit: Suit,
-  power?: number
-}
 const props = defineProps<Card>();
-const cardId = computed(() => `${props.rank.at(-1)}${suitsDictionary[props.suit]}`)
-
 const emit = defineEmits<{
   (e: "cardDrag", $event: Event, card: Card): void
   (e: "cardDragEnd", $event: Event, card: Card): void
 }>();
 
+const { id, isTrump, canBeUsedForAttack, canBeUsedForDefense, canBeUsedForTransferMove } = useGameCard(props);
 </script>
