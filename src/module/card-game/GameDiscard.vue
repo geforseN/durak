69b8cee9f303dfe858e0de0ae84f98edc2724b71
GeForsen- <semen.neb@gmail.com>
2023-05-12 11:10:00 +0700

    GameDiscard: init

---
+++ src/module/card-game/GameDiscard.vue
@@ -0,0 +1,9 @@
+<template>
+  <card-background v-if="!gameStateStore.gameState.isDiscardEmpty" />
+</template>
+<script setup lang="ts">
+import CardBackground from '@/module/card-game/CardBackground.vue';
+import { useGameStateStore } from '@/stores/game';
+
+const gameStateStore = useGameStateStore();
+</script>
