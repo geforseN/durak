7ec8e0a7f8aa90fb6b6eec631e0bb4ba3313508b
GeForsen- <semen.neb@gmail.com>
2023-05-12 11:53:30 +0700

    SelfDesk: update colors & wrap into button & use v-bind & invert if-else

--- src/module/card-game/SelfDeck.vue
+++ src/module/card-game/SelfDeck.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="w-full max-w-5xl">
-    <div v-if="selfStore.self.cards?.length > 0"
-      class="h-max border-black border-2 w-full p-2 flex bg-primary justify-center rounded">
-      <game-card
-        class="hover:scale-125 transition ease-out"
-        v-for="card of selfStore.self.cards"
-        :key="card.suit + card.rank"
-        :rank="card.rank"
-        :suit="card.suit"
-        @card-drag="handleCardDrag($event, card)"
-        @card-drag-end="handleCardDragEnd($event, card)"
-      />
-    </div>
-    <div v-else>У вас нет карт!</div>
+  <div class="flex w-full max-w-5xl h-max border-neutral-900 border-2 p-2 bg-neutral justify-center rounded">
+    <div v-if="!selfStore.self.cards.length" class="text-white">У вас нет карт!</div>
+    <template v-else>
+      <button v-for="card of selfStore.self.cards">
+        <game-card
+          class="hover:scale-125 transition ease-out"
+          :key="card.suit + card.rank"
+          v-bind="card"
+          @card-drag="handleCardDrag($event, card)"
+          @card-drag-end="handleCardDragEnd($event, card)" />
+      </button>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import GameCard, { type Card } from "@/module/card-game/Card.vue";
+import GameCard from "@/module/card-game/Card.vue";
 import { useGameSelfStore } from "@/stores/game/self.store";
+import type { Card } from "@/module/card-game/types";
 
 const emit = defineEmits<{
   (name: "cardDrag", $event: Event, card: Card): void
