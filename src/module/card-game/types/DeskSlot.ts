f7a6f32f0f7bd4757ce832041efc46ac08f1e6a4
GeForsen- <semen.neb@gmail.com>
2023-05-12 11:14:29 +0700

    types.DeskSlot: make properties optional

--- src/module/card-game/types/DeskSlot.ts
+++ src/module/card-game/types/DeskSlot.ts
@@ -1,6 +1,6 @@
 import type { Card } from "@/module/card-game/types/Card";
 
 export type DeskSlot = {
-  attackCard: Card | null,
-  defendCard: Card | null,
+  attackCard?: Card,
+  defendCard?: Card,
 }
