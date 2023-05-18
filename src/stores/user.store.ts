957814573f8eff775b0ce3f571b4eacb36f14baf
GeForsen- <semen.neb@gmail.com>
2023-05-18 14:25:58 +0700

    useUserStore: add lobby & game related refs

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { socket } from "@/socket";
 import { type User, ConnectStatus } from "@/module/global-chat/types";
 import generateNotificationFromError from "@/utils/generate-notification-from-error";
@@ -15,6 +15,9 @@ export const useUserStore = defineStore("user", () => {
   const photoUrl = ref("");
   const connectStatus = ref<ConnectStatus>(ConnectStatus.offline);
   const personalLink = ref("");
+  const currentLobbyId = ref<string | undefined>();
+  const currentGameId = ref<string | undefined>();
+  const isCreatingLobby = ref(false);
 
   socket.on("connect_error", (error) => {
     const notification = generateNotificationFromError(error);
@@ -35,6 +38,9 @@ export const useUserStore = defineStore("user", () => {
     photoUrl,
     connectStatus,
     personalLink,
+    currentGameId,
+    currentLobbyId,
+    isCreatingLobby,
   };
 });
 
