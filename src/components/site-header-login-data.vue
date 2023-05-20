15e00d21a1d81e8eacde5e1dfbac379664684736
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:28:07 +0700

    site-header-login-date: init

---
+++ src/components/site-header-login-data.vue
@@ -0,0 +1,33 @@
+<template>
+  <template v-if="username">
+    <li>
+      <router-link
+        :to="`/profile/${personalLink}`"
+        class="h-12 w-12 rounded border border-primary"
+        active-class="shadow-lg shadow-primary"
+      >
+        <img
+          :src="photoUrl"
+          :alt="`${username} profile picture`"
+          :title="username"
+          class="rounded"
+        />
+      </router-link>
+    </li>
+  </template>
+  <template v-else>
+    <list-item-link to="/auth/registration">Зарегистрироваться</list-item-link>
+    <li class="ml-2" />
+    <list-item-link to="/auth/login">Войти</list-item-link>
+  </template>
+</template>
+
+<script setup lang="ts">
+import ListItemLink from "@/components/list-item-link.vue";
+
+const { username, photoUrl, personalLink } = defineProps<{
+  username: string;
+  photoUrl: string;
+  personalLink: string;
+}>();
+</script>
