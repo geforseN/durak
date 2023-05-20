30e335922857b57e6464451d201ae94eef893932
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:57:47 +0700

    global-chat-messages: use store

--- src/module/global-chat/global-chat-messages.vue
+++ src/module/global-chat/global-chat-messages.vue
@@ -1,50 +1,48 @@
 <template>
   <section
-    class="flex-1 overflow-y-scroll scrollbar bg-info/70 border-neutral/70 border-2 rounded flex flex-col"
-    ref="messagesContainer">
+    class="scrollbar flex flex-1 flex-col overflow-y-scroll rounded border-2 border-neutral/70 bg-info/70"
+    ref="messagesContainer"
+  >
     <global-chat-user-message
-      v-for="message of messages"
-      :key="message.date+message.sender.accname"
+      v-for="message of globalChatStore.messages"
+      :key="message.date + message.sender.accname"
       :message="message"
     />
   </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { globalChat } from "@/socket";
-import GlobalChatUserMessage from "../global-chat/global-chat-user-message.vue";
-import type { UserMessage } from "@/module/global-chat/types";
-import formatTime from "@/utils/intl/format-time";
+import { onMounted, onUnmounted, ref, watch } from "vue";
+import { whenever } from "@vueuse/core";
+import { useGlobalChatStore } from "@/composable/useGlobalChatStore";
+import GlobalChatUserMessage from "./global-chat-user-message.vue";
 
-const messages = ref<UserMessage[]>([]);
 const messagesContainer = ref<HTMLElement>();
+const globalChatStore = useGlobalChatStore();
 
-const scrollToLastElement = ({ behavior = "auto" }: Omit<ScrollOptions, "top"> = {}) => {
+onMounted(globalChatStore.openSocket);
+onUnmounted(globalChatStore.closeSocket);
+whenever(
+  () => globalChatStore.isMessagesLoaded,
+  () => {
+    scrollToLastElement();
+  },
+);
+watch(
+  () => globalChatStore.messages,
+  () => {
+    scrollToLastElement({ behavior: "smooth" });
+  },
+  { deep: true },
+);
+
+function scrollToLastElement(
+  { behavior = "auto" }: Omit<ScrollOptions, "top"> = {},
+) {
   return queueMicrotask(() => {
     if (!messagesContainer.value) return;
     const top = messagesContainer.value.scrollHeight;
     messagesContainer.value?.scrollTo({ behavior, top });
   });
-};
-
-const getFormattedMessages = (unformattedMessages: UserMessage[]) => {
-  return unformattedMessages.map((message) => {
-    if (typeof message.date === "number") {
-      message.date = formatTime(message.date);
-    }
-    return message;
-  });
-};
-
-globalChat.on("sendMessage", (message: UserMessage) => {
-  message.date = formatTime(message.date as number);
-  messages.value.push(message);
-  scrollToLastElement({ behavior: "smooth" });
-});
-
-globalChat.on("restoreHistory", (globalChatHistory: UserMessage[]) => {
-  messages.value = getFormattedMessages(globalChatHistory);
-  scrollToLastElement();
-});
+}
 </script>
