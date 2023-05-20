ecaf44e587cc9bf953089a4982412d7cf61fea10
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:28:41 +0700

    list-item-link: init

---
+++ src/components/list-item-link.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <router-link
+      :to="to"
+      v-bind="attrs"
+      class="btn-ghost btn gap-2 text-xl text-secondary"
+      active-class="underline underline-offset-8 text-accent decoration-black"
+    >
+      <slot></slot>
+    </router-link>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+const { to } = defineProps<{
+  to: string
+}>();
+</script>
