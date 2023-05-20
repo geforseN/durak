5a30424f4897a49d47e6ea1b05ec82e6948693e6
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:20:24 +0700

    global-chat-user-message-avatar: init

---
+++ src/module/global-chat/global-chat-user-message-avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="chat-image avatar w-12" :class="isMyMessage ? 'hidden' : 'pl-2'">
+    <div class="rounded">
+      <img
+        :src="message.sender.photoUrl"
+        :alt="`${message.sender.nickname} profile picture`"
+      />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { UserMessage } from "./types";
+
+const { message, isMyMessage } = defineProps<{
+  message: UserMessage;
+  isMyMessage: boolean;
+}>();
+</script>
