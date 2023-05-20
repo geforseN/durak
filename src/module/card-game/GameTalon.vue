53154099e9acc8e3e05ffdad61126a7a94f17a89
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:50:34 +0700

    game-talon: update styles

--- src/module/card-game/GameTalon.vue
+++ src/module/card-game/GameTalon.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative">
+  <div class="relative right-5 xs:right-3 lg:right-2">
     <card-background v-if="!gameStateStore.gameState.isTalonHasOneCard && !gameStateStore.gameState.isTalonEmpty"
-      class="relative z-10" />
+      class="relative left-5 z-10" />
     <basic-card v-if="gameStateStore.gameState.trumpCard" v-bind="gameStateStore.gameState.trumpCard"
-      class="absolute top-0 right-6 rotate-90" :class="gameStateStore.gameState.isTalonEmpty && 'opacity-50'" />
+      class="absolute top-0 rotate-90" :class="gameStateStore.gameState.isTalonEmpty && 'opacity-30'" />
   </div>
 </template>
 
