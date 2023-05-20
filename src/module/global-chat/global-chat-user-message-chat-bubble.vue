206baef46d356cd8e367dfaa34d181ac16720fd5
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:20:43 +0700

    global-chat-user-message-chat-bubble: init

---
+++ src/module/global-chat/global-chat-user-message-chat-bubble.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="chat-bubble flex flex-col break-all p-2 pb-1"
+    :class="isMyMessage ? 'items-end bg-neutral-600' : 'items-start'"
+  >
+    <div class="flex flex-wrap items-baseline gap-1">
+      <span
+        class="badge rounded-sm border-0 bg-black p-1 font-bold"
+        :class="isMyMessage && 'hidden'"
+      >
+        {{ sender.nickname }}
+      </span>
+      <time class="text-xs opacity-50">{{ date }}</time>
+    </div>
+    <span class="ml-0.5">{{ text }}</span>
+  </div>
+</template>
+<script setup lang="ts">
+import type { UserMessage } from "./types";
+
+const { message, isMyMessage } = defineProps<{
+  isMyMessage: boolean;
+  message: UserMessage;
+}>();
+const { sender, date, text } = message;
+</script>
