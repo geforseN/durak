b240742990bde04c5e46d854e18001fabe2497bd
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:25:32 +0700

    global-chat-user-message: use components

--- src/module/global-chat/global-chat-user-message.vue
+++ src/module/global-chat/global-chat-user-message.vue
@@ -1,31 +1,19 @@
 <template>
   <div class="chat" :class="isMyMessage ? 'chat-end' : 'chat-start'">
-    <div class="w-12 avatar chat-image" :class="isMyMessage ? 'hidden' : 'pl-2 '">
-      <div class="rounded">
-        <img :src="sender.photoUrl" :alt="`${sender.nickname} profile picture`" />
-      </div>
-    </div>
-    <div class="chat-bubble break-all flex flex-col p-2 pb-1" :class="isMyMessage ? 'items-end bg-neutral-600' : 'items-start'">
-      <div class="flex flex-wrap gap-1 items-baseline">
-        <span class="font-bold badge border-0 bg-black rounded-sm p-1" :class="isMyMessage && 'hidden'">
-          {{ sender.nickname }}
-        </span>
-        <time class="text-xs opacity-50">{{ date }}</time>
-      </div>
-      <span class="ml-0.5">{{ text }}</span>
-    </div>
+    <global-chat-user-message-avatar v-bind="{ message, isMyMessage }" />
+    <global-chat-user-message-chat-bubble v-bind="{ message, isMyMessage }" />
   </div>
 </template>
-
 <script setup lang="ts">
 import type { UserMessage } from "@/module/global-chat/types";
 import { useUserStore } from "@/stores/user.store";
-import { computed } from "@vue/reactivity";
+import GlobalChatUserMessageChatBubble from "./global-chat-user-message-chat-bubble.vue";
+import GlobalChatUserMessageAvatar from "./global-chat-user-message-avatar.vue";
+import { computed } from "vue";
 
-const props = defineProps<{ message: UserMessage }>();
-const { date, text, sender } = props.message;
-
-const user = useUserStore();
-
-const isMyMessage = computed(() => user.accname === sender.accname)
+const { message } = defineProps<{ message: UserMessage }>();
+const userStore = useUserStore();
+const isMyMessage = computed(
+  () => userStore.accname === message.sender.accname,
+);
 </script>
