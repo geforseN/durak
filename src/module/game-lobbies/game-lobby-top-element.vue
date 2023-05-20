770c30c32560fc130d4ce71fa79ed81c49643036
GeForsen- <semen.neb@gmail.com>
2023-05-20 20:35:59 +0700

    game-lobby-top-element: add store usage & add leave button & update styles

--- src/module/game-lobbies/game-lobby-top-element.vue
+++ src/module/game-lobbies/game-lobby-top-element.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="flex flex-col sm:flex-row gap-1 justify-between">
-    <div class="flex sm:flex-1 gap-2 items-center">
-      <span class="badge badge-lg bg-neutral-700 border-black">
-        {{ gameTypesDictionary[props.lobby.settings.gameType] }}
+  <div class="flex flex-col justify-between gap-1 sm:flex-row">
+    <div class="flex items-center gap-2 sm:flex-1">
+      <span class="badge badge-lg border-black bg-neutral-700">
+        {{ gameTypesDictionary[lobby.settings.gameType] }}
       </span>
-      <span class="badge badge-lg bg-neutral-700 border-black">
-        {{ props.lobby.settings.cardCount }}
+      <span class="badge badge-lg border-black bg-neutral-700">
+        {{ lobby.settings.cardCount }}
       </span>
     </div>
     <button
-      v-if="props.lobby.adminAccname === userStore.accname"
-      class="btn btn-sm text-black bg-info hover:bg-info hover:saturate-[1.3] border-2 border-black"
-      @click="createGame()">
+      v-if="lobby.users.some((user) => user.accname === userStore.accname)"
+      class="btn-sm btn border-2 border-black bg-error text-black hover:bg-error/75"
+      @click="leaveLobby()"
+    >
+      Покинуть лобби
+    </button>
+    <button
+      v-if="lobby.adminAccname === userStore.accname && lobby.settings.maxUserCount === lobby.users.length"
+      class="btn-sm btn border-2 border-black bg-info text-black hover:bg-info hover:saturate-[1.3]"
+      @click="createGame(lobby.id)"
+    >
       Начать игру
     </button>
     <button
-      v-if="props.lobby.users.every(({ accname }) => accname !== userStore.accname)"
-      class="btn btn-sm text-black bg-success hover:bg-success hover:saturate-[1.3] border-black border-2"
-      @click="joinLobby()">
+      v-if="lobby.users.every(({ accname }) => accname !== userStore.accname)"
+      class="btn-sm btn border-2 border-black bg-success text-black hover:bg-success hover:saturate-[1.3]"
+      @click="joinLobby(lobby.id)"
+    >
       Присоединиться
     </button>
   </div>
@@ -26,18 +35,11 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user.store";
 import gameTypesDictionary from "../../utils/dictionary/game-types.dictionary";
-import { gameLobbies } from "@/socket";
 import type { Lobby } from "@/module/game-lobbies/types";
+import { useGameLobbiesStore } from "@/composable/useGameLobbiesStore";
 
 const userStore = useUserStore();
 
-const props = defineProps<{ lobby: Lobby }>();
-
-const joinLobby = (cellIndex: number = -1) => {
-  gameLobbies.emit("joinLobby", props.lobby.id, cellIndex);
-};
-
-const createGame = () => {
-  gameLobbies.emit("createGame", userStore.accname, props.lobby.id);
-};
+const { lobby } = defineProps<{ lobby: Lobby }>();
+const { joinLobby, leaveLobby, createGame } = useGameLobbiesStore();
 </script>
