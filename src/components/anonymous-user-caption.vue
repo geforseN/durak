510d03289c419e1a87de95b6b31642bd6d21eaa6
geforseN <semen.neb@gmail.com>
2023-09-10 21:22:16 +0700

    update user avatar and add anon user caption

---
+++ src/components/anonymous-user-caption.vue
@@ -0,0 +1,15 @@
+<template>
+  <li
+    class="bg-neutral-600 flex gap-x-2 account rounded border border-black px-2"
+  >
+    <div class="font-bold flex flex-wrap gap-x-1 justify-center max-w-[6rem]">
+      <span>Anonymous </span><span>account</span>
+    </div>
+    <div
+      class="border border-black rounded flex justify-center items-center bg-gray-400 w-5 h-5 tooltip tooltip-left tooltip-info"
+      data-tip="All game progress is not saved when playing in an anonymous account"
+    >
+      ?
+    </div>
+  </li>
+</template>
--- src/components/user-avatar.vue
+++ src/components/user-avatar.vue
@@ -1,29 +1,27 @@
 <template>
-  <li v-if="userStore.user.profile.nickname">
-    <div v-if="userStore.user.isAnonymous">
-      <div
-        class="tooltip"
-        data-tip="All game progress is not saved when playing in an anonymous account"
+  <template v-if="userStore.isUserDataLoaded">
+    <anonymous-user-caption
+      v-if="userStore.user.isAnonymous"
+    ></anonymous-user-caption>
+    <li class="ml-2">
+      <router-link
+        :to="`/profile/${userStore.user.profile.personalLink}`"
+        class="h-12 w-12 rounded border border-primary"
+        active-class="shadow-lg shadow-primary"
       >
-        <div>Anonymous account</div>
-      </div>
-    </div>
-    <router-link
-      :to="`/profile/${userStore.user.profile.personalLink}`"
-      class="h-12 w-12 rounded border border-primary"
-      active-class="shadow-lg shadow-primary"
-    >
-      <img
-        :src="userStore.user.profile.photoUrl"
-        :alt="`${userStore.user.profile.nickname} profile picture`"
-        :title="userStore.user.profile.nickname"
-        class="rounded"
-      />
-    </router-link>
-  </li>
+        <img
+          :src="userStore.user.profile.photoUrl"
+          :alt="`${userStore.user.profile.nickname} profile picture`"
+          :title="userStore.user.profile.nickname"
+          class="rounded"
+        />
+      </router-link>
+    </li>
+  </template>
 </template>
 <script lang="ts" setup>
 import { useUserStore } from "@/stores/user.store";
+import AnonymousUserCaption from "./anonymous-user-caption.vue";
 
 const userStore = useUserStore();
 await userStore.getMe();
