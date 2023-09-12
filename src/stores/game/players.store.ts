356f03ce7e43e6757c5a8aa226c95429ff289701
geforseN <semen.neb@gmail.com>
2023-09-12 17:48:16 +0700

    usePlayersStore: lint

--- src/stores/game/players.store.ts
+++ src/stores/game/players.store.ts
@@ -2,29 +2,27 @@ import { defineStore } from "pinia";
 import { computed } from "vue";
 import { useGameEnemiesStore, useGameSelfStore } from "./index";
 import type { BasePlayer, Card as CardDTO } from "@durak-game/durak-dts";
-export const usePlayersStore = defineStore(
-  "players-store",
-  () => {
+export const usePlayersStore = defineStore("players-store", () => {
   const enemiesStore = useGameEnemiesStore();
   const selfStore = useGameSelfStore();
 
-    const me = computed(() => "TODO");
-    const changeRole = () => "TODO";
+  const me = computed(() => "TODO");
+  const changeRole = () => "TODO";
 
-    const allowedPlayer = computed(() => {});
+  const allowedPlayer = computed(() => {});
 
-    const restore = (
-      selfDTO: BasePlayer & { cards: CardDTO[] },
-      enemyDTOs: (BasePlayer & { cardCount: number })[],
-    ) => {
-      selfStore.restore(selfDTO);
-      enemiesStore.restore(enemyDTOs);
-    };
-    
-    return {
-      restore,
-    };
-    ` 
+  const restore = (
+    selfDTO: BasePlayer & { cards: CardDTO[] },
+    enemyDTOs: (BasePlayer & { cardCount: number })[],
+  ) => {
+    selfStore.restore(selfDTO);
+    enemiesStore.restore(enemyDTOs);
+  };
+
+  return {
+    restore,
+  };
+  ` 
 has
 
 findBy
@@ -35,5 +33,4 @@ leftEnemies
 topEnemies
 rightEnemies
 `;
-  },
-);
+});
