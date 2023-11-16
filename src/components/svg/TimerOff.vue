53868fd7853555a001d68a3ce89fa60cf1d1ae3d
geforseN <semen.neb@gmail.com>
2023-11-16 16:51:10 +0700

    update svg components

---
+++ src/components/svg/TimerOff.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="24px"
+    height="24px"
+    stroke-width="1.5"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="#000000"
+  >
+    <path
+      d="M9 2h6M5 7l14 14.5M12 10v4M6.19 8.5a8 8 0 0011.05 11.544M19.42 17A8 8 0 009.21 6.5"
+      stroke="#000000"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
