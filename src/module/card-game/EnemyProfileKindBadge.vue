80906247bab266b9cc239919471bd2d538df93e3
geforseN <semen.neb@gmail.com>
2023-11-16 16:54:00 +0700

    EnemyProfileKindBadge: update

--- src/module/card-game/EnemyProfileKindBadge.vue
+++ src/module/card-game/EnemyProfileKindBadge.vue
@@ -1,17 +1,30 @@
 <template>
-  <shield-svg
-    class="w-6 h-6 z-10 border border-black rounded bg-blue-700"
-    v-if="kind === 'Defender'"
-  />
-  <swords-svg
-    class="w-6 h-6 z-10 border border-black rounded bg-red-700"
-    v-else-if="kind === 'Attacker'"
-  />
+  <div class="flex gap-x-0.5">
+    <shield-svg
+      class="w-6 h-6 border border-black rounded bg-blue-700"
+      v-if="kind.includes('Defender')"
+    />
+    <swords-svg
+      class="w-6 h-6 border border-black rounded bg-red-700"
+      v-else-if="kind.includes('Attacker')"
+    />
+    <div
+      class="w-6 h-6 border border-black rounded bg-green-700 flex justify-center items-center"
+      v-if="kind.includes('Allowed')"
+    >
+      <clock-svg v-if="hasTimer" class="w-6 h-6" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import ShieldSvg from "@/components/svg/Shield.vue";
 import SwordsSvg from "@/components/svg/Swords.vue";
+import ClockSvg from "@/components/svg/Clock.vue";
 import type { PlayerKind } from "./types";
-const { kind } = defineProps<{ kind: PlayerKind }>();
+
+const { kind, hasTimer } = defineProps<{
+  kind: PlayerKind;
+  hasTimer: boolean;
+}>();
 </script>
