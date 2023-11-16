f690845f8fda96817ccac5a1e2f8fc8fbefb09d5
geforseN <semen.neb@gmail.com>
2023-11-16 17:02:15 +0700

    SelfDeck: remove store usage

--- src/module/card-game/SelfDeck.vue
+++ src/module/card-game/SelfDeck.vue
@@ -2,12 +2,10 @@
   <div
     class="flex w-full max-w-5xl border-neutral-900 border-2 p-2 bg-neutral justify-center rounded"
   >
-    <div v-if="!selfStore.self.hasCards" class="text-white">
-      У вас нет карт!
-    </div>
+    <div v-if="isEmpty" class="text-white">У вас нет карт!</div>
     <div v-else class="max-w-xl grid grid-flow-col auto-cols-fr justify-center">
       <self-card
-        v-for="(card, index) of selfStore.self.cards"
+        v-for="(card, index) of cards"
         :key="card.suit + card.rank"
         :rank="card.rank"
         :suit="card.suit"
@@ -18,7 +16,10 @@
 </template>
 <script setup lang="ts">
 import SelfCard from "@/module/card-game/SelfCard.vue";
-import { useGameSelfStore } from "@/stores/game/self.store";
+import type { CardDTO } from "@durak-game/durak-dts";
 
-const selfStore = useGameSelfStore();
+const { cards, isEmpty } = defineProps<{
+  cards: CardDTO[];
+  isEmpty: boolean;
+}>();
 </script>
