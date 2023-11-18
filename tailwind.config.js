851f03b41c8c4c3796737373c616a96665001c31
geforseN <semen.neb@gmail.com>
2023-11-18 19:04:36 +0700

    tailwind: change daisyui theme and lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'xxs': '310px',
-      'xs': '475px', /* 
+      xxs: "310px",
+      xs: "475px" /* 
       'sm': '640px'
       'md': '768px'
       'lg': '1024px'
       'xl': '1280px'
-      '2xl': '1536px' */
+      '2xl': '1536px' */,
       ...defaultTheme.screens,
     },
     extend: {},
@@ -19,9 +19,9 @@ module.exports = {
   plugins: [
     require("daisyui"),
     require("tailwindcss-debug-screens"),
-    require("@savvywombat/tailwindcss-grid-areas")
+    require("@savvywombat/tailwindcss-grid-areas"),
   ],
   daisyui: {
-    themes: ["garden"],
+    themes: ["corporate"],
   },
 };
