f01717f6cafc392dce4fcdf57ae9ad0009f2fb9f
geforseN <semen.neb@gmail.com>
2023-11-19 18:40:25 +0700

    vite: add proxy for api route

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
       vue({
@@ -13,6 +14,15 @@ export default defineConfig(() => {
         },
       }),
     ],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_REST_BASE,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
