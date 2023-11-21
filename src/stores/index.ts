40e6d15c60246a60479aa288d7d8fd1f2c3b3f94
geforseN <semen.neb@gmail.com>
2023-11-21 22:28:20 +0700

    stores: add index file

---
+++ src/stores/index.ts
@@ -0,0 +1,5 @@
+export { useLobbiesStore } from "./lobbies.store";
+export { useNotificationStore } from "./notification.store";
+export { useGameLobbiesStore } from "./useGameLobbiesStore";
+export { useGlobalChatStore } from "./useGlobalChatStore";
+export { useUserStore } from "./user.store";
