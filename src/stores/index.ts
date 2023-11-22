55fcf6a03747c8bbd5801de62caf7e2baadb61c9
geforseN <semen.neb@gmail.com>
2023-11-22 13:57:00 +0700

    stores index: remove line

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,4 @@
 export { useLobbiesStore } from "./lobbies.store";
 export { useNotificationStore } from "./notification.store";
-export { useGameLobbiesStore } from "./useGameLobbiesStore";
 export { useGlobalChatStore } from "./useGlobalChatStore";
 export { useUserStore } from "./user.store";
