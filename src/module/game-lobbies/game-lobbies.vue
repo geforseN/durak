4c29c1029022a8f1f50aef0e6806e07c52d8b248
geforseN <semen.neb@gmail.com>
2023-11-22 21:23:36 +0700

    game-lobbies: update
    use lobbiesStore instead of gameLobbiesStore
    add components listeners
    lint imports

--- src/module/game-lobbies/game-lobbies.vue
+++ src/module/game-lobbies/game-lobbies.vue
@@ -3,26 +3,36 @@
     class="grid grid-cols-[minmax(250px,330px)] items-start justify-center min-[400px]:grid-cols-2 sm:grid-cols-1"
   >
     <div
-      v-for="lobby of gameLobbiesStore.lobbies"
+      v-for="lobby of lobbiesStore.state"
       :key="lobby.id"
       class="m-2 flex flex-col gap-2 rounded-lg border-4 p-1 shadow-xl sm:m-2 sm:w-[572px] md:w-[704px] lg:w-[836px]"
       :class="
-        userStore.user.currentLobbyId === lobby.id
+        userStore.user.state.currentLobbyId === lobby.id
           ? '-order-1 border-secondary-focus bg-primary'
           : 'border-primary bg-secondary-focus '
       "
     >
-      <game-lobby-top-element :lobby="lobby" />
-      <game-lobby-slots :lobby="lobby" />
+      <game-lobby-top-element
+        :lobby="lobby"
+        @join-lobby="lobbiesStore.ws.emits.joinLobby(lobby.id, -1)"
+        @leave-lobby="lobbiesStore.ws.emits.leaveLobby(lobby.id)"
+        @start-game="lobbiesStore.ws.emits.startGame(lobby.id)"
+      />
+      <game-lobby-slots
+        :lobby="lobby"
+        @join-lobby="
+          (slotIndex) => lobbiesStore.ws.emits.joinLobby(lobby.id, slotIndex)
+        "
+      />
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import GameLobbyTopElement from "@/module/game-lobbies/game-lobby-top-element.vue";
-import GameLobbySlots from "@/module/game-lobbies/game-lobby-slots.vue";
-import { useUserStore } from "@/stores/user.store";
-import { useGameLobbiesStore } from "@/stores/useGameLobbiesStore";
+import { useUserStore, useLobbiesStore } from "@/stores";
+
+import GameLobbyTopElement from "./game-lobby-top-element.vue";
+import GameLobbySlots from "./game-lobby-slots.vue";
 
 const userStore = useUserStore();
-const gameLobbiesStore = useGameLobbiesStore();
+const lobbiesStore = useLobbiesStore();
 </script>
