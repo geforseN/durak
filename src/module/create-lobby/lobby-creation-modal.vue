3cbfdd3ee182b276bdd77f6018751f27788959c5
geforseN <semen.neb@gmail.com>
2023-11-22 21:15:28 +0700

    lobby-creation-modal: update

--- src/module/create-lobby/lobby-creation-modal.vue
+++ src/module/create-lobby/lobby-creation-modal.vue
@@ -13,39 +13,49 @@
             class="btn btn-square bg-error transition-colors hover:bg-error hover:saturate-150"
           >
             <span class="sr-only">Отменить создание игровой комнаты</span>
-            <x-mark class="stroke-black stroke-[3] p-1" aria-hidden="true" />
+            <icons-xmark
+              class="stroke-black stroke-[3] p-1"
+              aria-hidden="true"
+            />
           </button>
         </form>
       </div>
       <radio-input-group
         name="player-count"
         class="my-2"
-        :input-values="allowedUserCount"
-        v-model.number="lobbySettings.userCount"
+        :input-values="lobbySettings.allowedValues.playerCount"
+        v-model.number="lobbySettings.state.value.playerCount"
       >
         Количество игроков
       </radio-input-group>
       <radio-input-group
         class="my-2"
         name="card-count"
-        :input-values="properCardCountValues"
-        v-model.number="lobbySettings.cardCount"
+        :input-values="lobbySettings.allowedValues.talonCardCount.value"
+        v-model.number="lobbySettings.state.value.talonCardCount"
       >
         Количество карт
       </radio-input-group>
       <select-group
         class="my-2"
         id="game-type"
-        :options="allowedGameTypes"
+        :options="lobbySettings.allowedValues.durakGameTypes"
         :options-dictionary="gameTypesDictionary"
-        v-model="lobbySettings.gameType"
+        v-model="lobbySettings.state.value.gameType"
       >
         Тип игры
       </select-group>
       <form method="dialog">
         <button
           class="btn btn-accent btn-lg h-auto w-full text-3xl"
-          @click="createLobby()"
+          @click="
+            try {
+              lobbySettings.assertValidLobbySettings();
+              emit('create-lobby', lobbySettings.state.value);
+            } catch {
+              emit('failed-create-lobby');
+            }
+          "
         >
           Создать комнату
         </button>
@@ -53,7 +63,7 @@
       <button
         type="reset"
         class="mx-auto my-1 w-fit px-2 py-0.5 text-base"
-        @click.prevent="resetSettings()"
+        @click.prevent="lobbySettings.resetToDefault()"
       >
         Сброс параметров
       </button>
@@ -64,41 +74,24 @@
   </dialog>
 </template>
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { ref } from "vue";
+import type { InitialGameSettings } from "@durak-game/durak-dts";
 
-import XMark from "@/components/svg/XMark.vue";
+import IconsXmark from "@/components/svg/XMark.vue";
+import gameTypesDictionary from "@/utils/dictionary/game-types.dictionary";
+
+import useLobbySettings from "./useLobbySettings";
 import RadioInputGroup from "./lobby-creation-radio-input-group.vue";
 import SelectGroup from "./lobby-creation-select.vue";
 
-import gameTypesDictionary from "@/utils/dictionary/game-types.dictionary";
+const lobbySettings = useLobbySettings();
 
-import useLobbySettings, {
-  allowedGameTypes,
-  allowedUserCount,
-} from "./useLobbySettings";
-import { useNotificationStore } from "@/stores/notification.store";
-import { useGameLobbiesStore } from "@/stores/useGameLobbiesStore";
+const emit = defineEmits<{
+  "create-lobby": [InitialGameSettings],
+  "failed-create-lobby": [],
+}>();
 
 const modalRef = ref<HTMLDialogElement | null>(null);
+
 defineExpose({ modalRef });
-
-const {
-  lobbySettings,
-  properCardCountValues,
-  resetSettings,
-  isProperCardCount,
-} = useLobbySettings();
-const { addNotificationInQueue } = useNotificationStore();
-
-const gameLobbiesStores = useGameLobbiesStore();
-
-const createLobby = () => {
-  if (!isProperCardCount()) {
-    return addNotificationInQueue({
-      durationInMS: 7_000,
-      message: `Нельзя создать лобби \n с количеством карт: ${lobbySettings.cardCount}\n и количеством карт: ${lobbySettings.userCount}`,
-    });
-  }
-  gameLobbiesStores.createLobby(lobbySettings);
-};
 </script>
