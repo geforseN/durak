f3e62f19823c209f8be1156da3c72479941b6594
geforseN <semen.neb@gmail.com>
2023-11-23 17:36:24 +0700

    players.store: remove dead code

--- src/stores/game/players.store.ts
+++ src/stores/game/players.store.ts
@@ -1,15 +1,17 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
-import { useGameEnemiesStore, useGameSelfStore } from "./index";
+import { computed } from "vue";
 import { array, enumType, is, number, object, optional, string } from "valibot";
-import { playerKinds, ranks, suits } from "@durak-game/durak-dts";
-import type {
-  BasePlayer,
-  Card as CardDTO,
-  DurakGameSocket,
-} from "@durak-game/durak-dts";
-import type Player from "@/module/card-game/entity/Player";
 import type { Socket } from "socket.io-client";
+import {
+  playerKinds,
+  ranks,
+  suits,
+  type BasePlayer,
+  type CardDTO,
+  type DurakGameSocket,
+} from "@durak-game/durak-dts";
+
+import { useGameEnemiesStore, useGameSelfStore } from "./index";
 
 const cardType = object({ rank: enumType(ranks), suit: enumType(suits) });
 const playerKindType = enumType(playerKinds);
@@ -64,25 +66,7 @@ export const useGamePlayersStore = defineStore("players-store", () => {
   const enemiesStore = useGameEnemiesStore();
   const selfStore = useGameSelfStore();
 
-  const __allowedPlayer__ = computed<Player>(
-    () => {
-      if (selfStore.self.isAllowed) {
-        return selfStore.self;
-      }
-      if (enemiesStore.enemies.hasAllowedPlayer) {
-        return enemiesStore.enemies.allowedPlayer;
-      }
-      throw new Error("No allowed player was found");
-    },
-    {
-      onTrigger(event) {
-        console.log("__allowedPlayer__", event);
-      },
-    },
-  );
-  const timerId = ref();
-
-  // TODO - wont work
+  // NOTE: it might now work, (idk why, must prev comment here was not helpful with that)
   const hasSurrenderedDefender = computed(() => {
     return (
       selfStore.self.isSurrendered ||
@@ -106,13 +90,10 @@ export const useGamePlayersStore = defineStore("players-store", () => {
   const changePlayerKind = ({ player }: { player: unknown }) => {
     const [enemy, self] = schemas["player::changedKind"];
     if (is(enemy, player)) {
-      return enemiesStore.enemies
-        .getById(player.id)
-        .updateKindWithTimer(player.newKind);
+      return enemiesStore.enemies.getById(player.id).updateKind(player.newKind);
     }
     if (is(self, player)) {
       return selfStore.self.updateKind(player.newKind);
-      // return selfStore.self.updateKindWithTimer(player.newKind);
     }
     throw new Error(`player::changedKind incorrect payload:${player}`);
   };
@@ -127,8 +108,6 @@ export const useGamePlayersStore = defineStore("players-store", () => {
   ) => {
     selfStore.restore(selfDTO, gameSocket);
     enemiesStore.restore(enemyDTOs);
-    addTimer();
-    1;
   };
 
   const removeCardFromPlayer = (payload: object) => {
@@ -149,52 +128,14 @@ export const useGamePlayersStore = defineStore("players-store", () => {
     if (!is(self, payload) && !is(enemy, payload)) {
       throw new Error("Invalid payload allowedPlayer::defaultBehavior");
     }
-    clearInterval(timerId.value);
-
-    __allowedPlayer__.value.timer = {
-      endTime: payload.defaultBehavior.callTime,
-    };
-
-    addTimer();
-  };
-
-  function addTimer() {
-    timerId.value = setInterval(() => {
-      console.log(__allowedPlayer__.value.id);
-      // console.log(__allowedPlayer__.value.timer);
-      // console.log(
-      //   (__allowedPlayer__.value.timer.endTime.UTC - Date.now()) / 1_000,
-      // );
-      // if (__allowedPlayer__.value.timer.endTime.UTC) {
-      //   return;
-      // }
-      //   __allowedPlayer__.value.timer.remainedTime.milliseconds =
-      //     __allowedPlayer__.value.timer.endTime.UTC - Date.now();
-      // __allowedPlayer__.value.timer.remainedTime.seconds =
-      //   __allowedPlayer__.value.timer.remainedTime.milliseconds / 1_000;
-
-      // __allowedPlayer__.value.timer.remainedTime.positiveTimeAsString =
-      //   Math.max(
-      //     __allowedPlayer__.value.timer.remainedTime.seconds,
-      //     0,
-      //   ).toExponential(2);
-      // selfStore.timer.remainedTime.timeAsString =
-      //   __allowedPlayer__.value.timer.remainedTime.seconds.toExponential(2);
-    }, 1_000);
-  }
-
-  const isPlayerAllowed = (player: Player) => {
-    return player.id === __allowedPlayer__.value?.id;
   };
 
   return {
+    restore,
     hasSurrenderedDefender,
-    __allowedPlayer__,
-    isPlayerAllowed,
     putCardsToPlayer,
     changePlayerKind,
     removeCardFromPlayer,
     updateTimerForNewAllowedPlayer,
-    restore,
   };
 });
