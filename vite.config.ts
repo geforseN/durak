4b201ce56f0104438348503b64f0fd9fb94fced5
geforseN <semen.neb@gmail.com>
2023-11-24 15:40:56 +0700

    add log in vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  console.log({ VITE_SERVER_REST_BASE: env.VITE_SERVER_REST_BASE });
   return {
     plugins: [
       vue({
