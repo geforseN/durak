4ef931b0d61878ad5aa099ad4e165fbe11cf8c2a
geforseN <geforsenjs@gmail.com>
2024-12-03 01:20:40 +0700

    ci(playwright): run on successful deployment
    style
    reduce timeout
    rename env variable
    do no run webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config';
 
-const baseURL = process.env.VITE_BASE_URL;
+const baseURL = process.env.PLAYWRIGHT_BASE_URL;
 if (!baseURL) {
-  throw new Error('VITE_BASE_URL environment variable is not defined');
+  throw new Error('PLAYWRIGHT_BASE_URL environment variable is not defined');
 }
 
 /**
@@ -69,7 +69,7 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  webServer: process.env.CI ? undefined : {
     command: 'pnpm dev',
     url: baseURL,
     reuseExistingServer: !process.env.CI,
