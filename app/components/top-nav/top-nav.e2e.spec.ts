58ac02914d91fa59301a90b0c8a6b5421c42c4d1
geforseN <geforsenjs@gmail.com>
2024-12-14 20:38:04 +0700

    test: top-nav biggerMobileWidth

--- app/components/top-nav/top-nav.e2e.spec.ts
+++ app/components/top-nav/top-nav.e2e.spec.ts
@@ -2,11 +2,25 @@ import { test, expect } from "@playwright/test";
 import screens from "../../../tailwind.screens";
 
 test.describe("TopNav", () => {
+  const mobileHeight = 568; /* can be other number */
+  const mobileWidth = parseInt(screens["xxs"], 10);
+
   test("mobile-like screen", async ({ page }) => {
     await page.goto("/");
     await page.setViewportSize({
-      width: parseInt(screens["xxs"], 10),
-      height: 568,
+      width: mobileWidth,
+      height: mobileHeight,
+    });
+    await expect(page.getByTestId("top-nav")).toHaveScreenshot();
+  });
+
+  const biggerMobileWidth = mobileWidth + 20;
+
+  test("bigger mobile-like screen", async ({ page }) => {
+    await page.goto("/");
+    await page.setViewportSize({
+      width: biggerMobileWidth,
+      height: mobileHeight,
     });
     await expect(page.getByTestId("top-nav")).toHaveScreenshot();
   });
