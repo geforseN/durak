4a6700e04f0c3c5e28990b82f0cd0d8e95a4520a
geforseN <geforsenjs@gmail.com>
2024-12-14 16:44:44 +0700

    chore(config): [eslint] update

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
-import pluginVue from "eslint-plugin-vue";
+// @ts-check
+import vue from "eslint-plugin-vue";
+import typescript from "typescript-eslint";
+import vitest from "@vitest/eslint-plugin";
 
-export default [...pluginVue.configs["flat/recommended"]];
+export default typescript.config(
+  { ignores: ["server/*"] },
+  vue.configs["flat/recommended"],
+  vitest.configs.recommended,
+);
