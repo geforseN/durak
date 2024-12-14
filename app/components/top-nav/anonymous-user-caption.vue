db70442eb87063c57414b85a11a90846ce850643
geforseN <geforsenjs@gmail.com>
2024-12-14 18:46:17 +0700

    refactor(components): [top-nav] make responsive

--- app/components/top-nav/anonymous-user-caption.vue
+++ app/components/top-nav/anonymous-user-caption.vue
@@ -1,13 +1,13 @@
 <template>
   <li class="contents">
     <div
-      class="flex h-12 items-center gap-x-2 rounded border border-black px-2"
+      class="flex h-8 items-center xs:gap-2 rounded border border-black px-1 xs:px-2 text-xs xs:h-12 xs:text-base"
     >
       <div class="flex max-w-[6rem] flex-wrap justify-center gap-x-1 font-bold">
         <span>Anonymous&nbsp;</span><span>account</span>
       </div>
       <div
-        class="tooltip tooltip-left tooltip-info flex h-5 w-5 items-center justify-center rounded border border-black bg-black text-white"
+        class="tooltip tooltip-left tooltip-info flex h-5 w-5 before:max-w-[12rem] before:xs:top-6 before:top-5 items-center justify-center rounded border border-black bg-black text-white before:text-xs before:xs:text-base"
         data-tip="All game progress is not saved when playing in an anonymous account"
       >
         ?
--- app/components/top-nav/user-avatar.vue
+++ app/components/top-nav/user-avatar.vue
@@ -5,7 +5,7 @@
       method="post"
     >
       <button
-        class="btn btn-accent"
+        class="btn btn-accent btn-sm xs:btn-md"
         :class="isCreatingAnonymousAccount && 'loading'"
         type="submit"
         @click="isCreatingAnonymousAccount = true"
@@ -14,27 +14,29 @@
       </button>
     </form>
   </template>
-  <template v-else-if="userStore.user.state">
+  <div class="contents" v-else-if="userStore.user.state">
     <anonymous-user-caption v-if="userStore.user.state.isAnonymous" />
     <list-item-link
       v-if="userStore.user.state.profile"
       :to="`/profile/${userStore.user.state.profile.personalLink}`"
-      class="ml-2 rounded border border-primary p-0"
+      class="btn btn-sm xs:btn-md btn-square btn-outline gap-2 rounded border border-primary p-0 align-middle"
       active-class="shadow-lg shadow-primary"
     >
       <img
-        width="48"
-        height="48"
+        :width="isExtraSmall ? 36 : 48"
+        :height="isExtraSmall ? 36 : 48"
         :src="userStore.user.state.profile.photoUrl"
         :alt="`${userStore.user.state.profile.nickname} profile picture`"
         :title="userStore.user.state.profile.nickname"
         class="rounded"
       >
     </list-item-link>
-  </template>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
+import { useBreakpoints } from "@vueuse/core";
+import breakpointsTailwind from "../../../tailwind.screens";
 
 import AnonymousUserCaption from "@/components/top-nav/anonymous-user-caption.vue";
 import ListItemLink from "@/components/top-nav/list-item-link.vue";
@@ -44,4 +46,7 @@ import { CREATE_ANON_USER_URL } from "@/api/rest";
 
 const isCreatingAnonymousAccount = ref(false);
 const userStore = useUserStore();
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const isExtraSmall = breakpoints.smallerOrEqual("xs");
 </script>
