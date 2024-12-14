b2d1ed445fa8c8924179eb39ca81099503d5c030
geforseN <geforsenjs@gmail.com>
2024-12-14 16:59:34 +0700

    chore(config): [eslint] ignore gitignore content

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
+import path from "node:path";
 import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import typescript from "typescript-eslint";
 import vitest from "@vitest/eslint-plugin";
+import * as compat from "@eslint/compat";
 import globals from "globals";
 
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
+
 export default typescript.config(
+  compat.includeIgnoreFile(gitignorePath),
   { ignores: ["server/*"] },
   { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
   {
