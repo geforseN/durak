13eee85d50c00e15eb09dbfc0ebb491f48bf2f47
geforseN <geforsenjs@gmail.com>
2024-12-14 18:46:17 +0700

    refactor(components): [top-nav] make responsive

--- app/components/top-nav/top-nav.vue
+++ app/components/top-nav/top-nav.vue
@@ -1,14 +1,16 @@
 <template>
   <nav
-    class="navbar sticky top-0 z-10 flex max-h-16 items-end border-b border-b-neutral bg-primary/50"
+    class="navbar gap-1 xs:gap-2 sticky top-0 z-10 flex h-12 min-h-0 items-end border-b border-b-neutral bg-primary/50 xs:h-16"
   >
     <ul class="contents">
-      <list-item-link to="/">
+      <list-item-link
+        class="btn btn-outline btn-sm border align-middle xs:btn-md"
+        to="/"
+      >
         <icons-home />
       </list-item-link>
       <li class="ml-auto" />
-      <app-theme-select />
-      <li class="mr-2" />
+      <app-theme-select class="select-sm xs:select-md" />
       <user-avatar />
     </ul>
   </nav>
