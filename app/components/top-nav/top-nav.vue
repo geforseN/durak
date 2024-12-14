894bcc0314685689abfca229062e0392a3902337
geforseN <geforsenjs@gmail.com>
2024-12-14 17:53:05 +0700

    refactor(components): [top-nav]: remove game related

--- app/components/top-nav/top-nav.vue
+++ app/components/top-nav/top-nav.vue
@@ -7,47 +7,6 @@
         <icons-home />
       </list-item-link>
       <li class="ml-auto" />
-      <list-item-link
-        v-if="userStore.user.state.currentGameId"
-        :to="`/game/${userStore.user.state.currentGameId}`"
-      >
-        Ваша игра
-      </list-item-link>
-      <li
-        v-else-if="userStore.user.state.currentLobbyId"
-        class="contents"
-      >
-        <button
-          class="btn btn-ghost gap-2 text-xl text-secondary"
-          @click="
-            lobbiesStore.ws.emits.leaveLobby(
-              userStore.user.state.currentLobbyId,
-            )
-          "
-        >
-          <icons-xmark class="h-6 w-6" />
-          Покинуть лобби
-        </button>
-      </li>
-      <template v-else>
-        <button
-          class="btn btn-primary text-xl transition-colors"
-          @click="lobbyCreationModal?.modalRef?.show()"
-        >
-          <icons-plus />
-          Создать игру
-        </button>
-
-        <teleport to="#app">
-          <lobby-creation-modal
-            ref="lobbyCreationModal"
-            @create-lobby="
-              (settings) => lobbiesStore.ws.emits.createLobby(settings)
-            "
-          />
-        </teleport>
-      </template>
-      <li class="mr-2" />
       <app-theme-select />
       <li class="mr-2" />
       <user-avatar />
@@ -55,21 +14,9 @@
   </nav>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-
 import AppThemeSelect from "@/components/app-theme-select.vue";
 import ListItemLink from "@/components/top-nav/list-item-link.vue";
 import UserAvatar from "@/components/top-nav/user-avatar.vue";
 
 import IconsHome from "@/components/svg/Home.vue";
-import IconsXmark from "@/components/svg/XMark.vue";
-import IconsPlus from "@/components/svg/Plus.vue";
-
-import { useUserStore, useLobbiesStore } from "@/stores";
-import LobbyCreationModal from "$/create-lobby/lobby-creation-modal.vue";
-
-const lobbyCreationModal = ref<InstanceType<typeof LobbyCreationModal>>();
-
-const lobbiesStore = useLobbiesStore();
-const userStore = useUserStore();
 </script>
