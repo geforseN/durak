49ab126786015781242c45488327c9022cb4ee0b
geforseN <geforsenjs@gmail.com>
2024-12-14 16:50:41 +0700

    fix(config): [eslint] vue files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
 // @ts-check
+import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import typescript from "typescript-eslint";
 import vitest from "@vitest/eslint-plugin";
+import globals from "globals";
 
 export default typescript.config(
   { ignores: ["server/*"] },
-  vue.configs["flat/recommended"],
+  { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      typescript.configs.recommended,
+      vue.configs["flat/recommended"],
+    ],
+    files: ["**/*.{ts,vue}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescript.parser,
+      },
+    },
+  },
   vitest.configs.recommended,
 );
