0279db542930ccdd8636a858d2c326de24621195
geforseN <geforsenjs@gmail.com>
2024-12-15 21:06:24 +0700

    chore: [App] use `WithAppDrawer` component

--- app/App.vue
+++ app/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="flex min-h-screen flex-col">
-    <top-nav
-      :class="
-        router.currentRoute.value.path.match(uuidRegExp) && 'bg-purple-400'
-      "
-    />
-    <router-view />
-    <notification-queue />
-  </div>
+  <with-app-drawer>
+    <div class="flex min-h-screen flex-col">
+      <top-nav
+        :class="
+          router.currentRoute.value.path.match(uuidRegExp) && 'bg-purple-400'
+        "
+      />
+      <router-view />
+      <notification-queue />
+    </div>
+  </with-app-drawer>
 </template>
-
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-
 import TopNav from "@/components/top-nav/top-nav.vue";
 import NotificationQueue from "@/components/notification-queue.vue";
+import WithAppDrawer from "$/app-drawer/components/with-app-drawer.vue";
 
 const router = useRouter();
 const uuidRegExp =
