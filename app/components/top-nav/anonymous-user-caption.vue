1231c1b10123c563d4c863842fe57d188a000a5d
geforseN <geforsenjs@gmail.com>
2024-12-15 16:52:06 +0700

    refactor(components): [anonymous-user-caption] change z-index
    remove li as root component
    add tooltipSide prop
    add fixme comment
    lint tailwind classes

--- app/components/top-nav/anonymous-user-caption.vue
+++ app/components/top-nav/anonymous-user-caption.vue
@@ -1,17 +1,31 @@
 <template>
-  <li class="contents">
-    <div
-      class="flex h-8 items-center xs:gap-2 rounded border border-black px-1 xs:px-2 text-xs xs:h-12 xs:text-base"
-    >
-      <div class="flex max-w-[6rem] flex-wrap justify-center gap-x-1 font-bold">
-        <span>Anonymous&nbsp;</span><span>account</span>
-      </div>
-      <div
-        class="tooltip tooltip-left tooltip-info flex h-5 w-5 before:max-w-[12rem] before:xs:top-6 before:top-5 items-center justify-center rounded border border-black bg-black text-white before:text-xs before:xs:text-base"
-        data-tip="All game progress is not saved when playing in an anonymous account"
-      >
-        ?
-      </div>
+  <div
+    class="flex h-8 items-center rounded border border-black px-1 text-xs xs:h-12 xs:gap-2 xs:px-2 xs:text-base"
+  >
+    <div class="flex max-w-[6rem] flex-wrap justify-center gap-x-1 font-bold">
+      <span>Anonymous&nbsp;</span><span>account</span>
     </div>
-  </li>
+    <div
+      class="tooltip tooltip-info flex h-5 w-5 items-center justify-center rounded border border-black bg-black text-white before:top-5 before:max-w-[12rem] before:text-xs before:xs:top-6 before:xs:text-base"
+      :class="tooltipSideClasses[tooltipSide]
+      "
+      data-tip="All game progress is not saved when playing in an anonymous account"
+    >
+      ?
+    </div>
+  </div>
 </template>
+<script lang="ts">
+const tooltipSideClasses = {
+  left: "tooltip-left",
+  right: "tooltip-right",
+  top: "tooltip-top",
+  bottom: "tooltip-bottom",
+}
+</script>
+<!-- FIXME: i18n -->
+<script setup lang="ts">
+const { tooltipSide = "left" } = defineProps<{
+  tooltipSide?: "left" | "right" | "top" | "bottom";
+}>();
+</script>
