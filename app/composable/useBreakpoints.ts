77c7c4eb02d206baccd95d0f912436f489fd9eae
geforseN <geforsenjs@gmail.com>
2024-12-15 16:55:22 +0700

    feat(composables): [useBreakpoints] init

---
+++ app/composable/useBreakpoints.ts
@@ -0,0 +1,6 @@
+import { useBreakpoints as _useBreakpoints } from "@vueuse/core";
+import screens from "../../tailwind.screens";
+
+export function useBreakpoints() {
+  return _useBreakpoints(screens);
+}
