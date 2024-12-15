ebe4a6efb85818c153bfca5f145b96246b7d653a
geforseN <geforsenjs@gmail.com>
2024-12-16 00:54:03 +0700

    feat(layers): [app-drawer] load content lazily

--- app/stores/useAppStore.ts
+++ app/stores/useAppStore.ts
@@ -1,10 +1,14 @@
-import { useAppScreen } from "@/composable/useAppScreen";
 import { defineStore } from "pinia";
+import { useAppScreen } from "@/composable/useAppScreen";
+import { useAppDrawer } from "$/app-drawer/composables/useAppDrawer";
 
 export const useAppStore = defineStore("app", () => {
   const screen = useAppScreen();
 
+  const drawer = useAppDrawer();
+
   return {
     screen,
+    drawer,
   };
 });
