cf1c98f47788f0222f32fe4583493eeba0b5b2f9
geforseN <geforsenjs@gmail.com>
2024-12-15 15:39:33 +0700

    feat(layers): [app-drawer] init

---
+++ layers/app-drawer/components/app-drawer-close-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <label
+    for="app-drawer"
+    aria-label="close sidebar"
+    class="btn btn-square btn-error btn-sm xs:btn-md"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="appScreen.isExtraSmall.value ? 24 : 40"
+      :height="appScreen.isExtraSmall.value ? 24 : 40"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"
+      />
+    </svg>
+  </label>
+</template>
+<script setup lang="ts">
+import { useAppScreen } from "@/composable/useAppScreen";
+
+const appScreen = useAppScreen();
+</script>
---
+++ layers/app-drawer/components/app-drawer-open-burger-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <label
+    for="app-drawer"
+    aria-label="open sidebar"
+    class="btn btn-square btn-ghost btn-sm xs:btn-md"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="appScreen.isExtraSmall.value ? 24 : 40"
+      :height="appScreen.isExtraSmall.value ? 24 : 40"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-width="1.5"
+        d="M20 7H4m16 5H4m16 5H4"
+      />
+    </svg>
+  </label>
+</template>
+<script setup lang="ts">
+import { useAppScreen } from "@/composable/useAppScreen";
+
+const appScreen = useAppScreen();
+</script>
