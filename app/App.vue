f0ed4bc404529dc4c834d20f614ee545c143cd49
geforseN <geforsenjs@gmail.com>
2024-12-15 21:07:13 +0700

    chore: [App] add `isGamePage` component

--- app/App.vue
+++ app/App.vue
@@ -1,17 +1,14 @@
 <template>
   <with-app-drawer>
     <div class="flex min-h-screen flex-col">
-      <top-nav
-        :class="
-          router.currentRoute.value.path.match(uuidRegExp) && 'bg-purple-400'
-        "
-      />
+      <top-nav :class="isGamePage && 'bg-purple-400'" />
       <router-view />
       <notification-queue />
     </div>
   </with-app-drawer>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import { useRouter } from "vue-router";
 import TopNav from "@/components/top-nav/top-nav.vue";
 import NotificationQueue from "@/components/notification-queue.vue";
@@ -20,4 +17,8 @@ import WithAppDrawer from "$/app-drawer/components/with-app-drawer.vue";
 const router = useRouter();
 const uuidRegExp =
   /^\/game\/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+
+const isGamePage = computed(() =>
+  router.currentRoute.value.path.match(uuidRegExp),
+);
 </script>
