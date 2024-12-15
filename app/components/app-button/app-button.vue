440f0be4618c186220072c2c0197b7075cc06b91
geforseN <geforsenjs@gmail.com>
2024-12-15 21:01:20 +0700

    chore(components): [app-button] init

---
+++ app/components/app-button/app-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button
+    class="btn"
+    :class="[
+      variant ? (variantClasses[variant] ?? '') : '',
+      size ? (sizeClasses[size ?? defaultSize] ?? '') : (defaultSize ?? ''),
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+<script lang="ts">
+const variantClasses = {
+  neutral: "btn-neutral",
+  primary: "btn-primary",
+  secondary: "btn-secondary",
+  accent: "btn-accent",
+  ghost: "btn-ghost",
+  link: "btn-link",
+};
+
+const sizeClasses = {
+  "extra-small": "btn-xs",
+  small: "btn-sm",
+  medium: "btn-md",
+  large: "btn-lg",
+};
+
+export type AppButtonProps = {
+  variant?: keyof typeof variantClasses;
+  size?: keyof typeof sizeClasses;
+  defaultSize?: keyof typeof sizeClasses;
+};
+</script>
+<script setup lang="ts">
+const { defaultSize } = defineProps<AppButtonProps>();
+</script>
