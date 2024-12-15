825b80b8d460028a15cff364b0d19d91357cde09
geforseN <geforsenjs@gmail.com>
2024-12-15 15:39:33 +0700

    feat(layers): [app-drawer] init

---
+++ layers/app-drawer/components/with-app-drawer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="drawer drawer-end">
+    <input
+      id="app-drawer"
+      type="checkbox"
+      class="drawer-toggle"
+    >
+    <div class="drawer-content">
+      <slot />
+    </div>
+    <div
+      class="drawer-side z-[2]"
+      v-bind="$attrs"
+    >
+      <label
+        for="app-drawer"
+        aria-label="close sidebar"
+        class="drawer-overlay"
+      />
+      <slot name="content" />
+    </div>
+  </div>
+</template>
---
+++ layers/app-drawer/index.ts
@@ -0,0 +1,3 @@
+export { default as WithAppDrawer } from "./components/with-app-drawer.vue";
+export { default as AppDrawerOpenBurgerButton } from "./components/app-drawer-open-burger-button.vue";
+export { default as AppDrawerCloseButton } from "./components/app-drawer-close-button.vue";
