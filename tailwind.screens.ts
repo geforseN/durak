2813ba5da980eadb45d82a0229a9119d6df774a5
geforseN <geforsenjs@gmail.com>
2024-12-16 14:39:01 +0700

    fix: playwright fails to run

--- tailwind.screens.ts
+++ tailwind.screens.ts
@@ -1,4 +1,4 @@
-import defaultTheme from "tailwindcss/defaultTheme";
+import defaultTheme from "tailwindcss/defaultTheme.js";
 
 /** @type {NonNullable<import('tailwindcss').Config['theme']>['screens']} */
 export default {
