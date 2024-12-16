532b1428d8384efb14a0612ddbdfdd5099cc0b9d
geforseN <geforsenjs@gmail.com>
2024-12-16 16:11:46 +0700

    chore(layers): [app-theme-select] add index file

---
+++ layers/app-theme-select/index.ts
@@ -0,0 +1,3 @@
+export { useAppTheme } from "$/app-theme-select/useAppTheme";
+export { appThemes, isAppTheme } from "$/app-theme-select/config/app-themes";
+export { AppThemeLocaleStorageRepository } from "$/app-theme-select/utils/locale-storage-repository";
