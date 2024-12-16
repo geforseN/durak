b04d64cddcfef44da93e310457b1a0e3bb9edc03
geforseN <geforsenjs@gmail.com>
2024-12-16 16:05:36 +0700

    chore(layers): [app-theme-select] add AppThemeRepository interface

---
+++ layers/app-theme-select/utils/locale-storage-repository.ts
@@ -0,0 +1,27 @@
+import type { AppThemeRepository } from "./repository.interface";
+
+export class AppThemeLocaleStorageRepository<T extends string>
+  implements AppThemeRepository<T>
+{
+  constructor(
+    readonly key: string,
+    readonly isAllowedValue: (theme: unknown) => theme is T,
+    readonly defaultValue: T,
+  ) {}
+
+  getOrDefault(): T {
+    const themeFromStorage = localStorage.getItem(this.key);
+    if (themeFromStorage && this.isAllowedValue(themeFromStorage)) {
+      return themeFromStorage;
+    }
+    return this.defaultValue;
+  }
+
+  setOrThrow(theme: unknown) {
+    if (this.isAllowedValue(theme)) {
+      localStorage.setItem(this.key, theme);
+    } else {
+      throw new Error(`Theme ${theme} is not allowed`);
+    }
+  }
+}
