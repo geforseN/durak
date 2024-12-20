35a3e99938e1f7da80a4f989c2fb7f46d709d5d3
geforseN <geforsenjs@gmail.com>
2024-12-20 12:47:34 +0700

    chore(router): [utils] init with getRouteStringParam

---
+++ app/router/utils.ts
@@ -0,0 +1,20 @@
+import { useRoute } from "vue-router";
+
+export function getRouteStringParam(key: string, transform?: undefined): string;
+export function getRouteStringParam<T>(
+  key: string,
+  transform: (value: string) => T,
+): T;
+export function getRouteStringParam<T>(
+  key: string,
+  transform?: (value: string) => T,
+) {
+  const value = useRoute().params[key];
+  if (typeof value !== "string") {
+    throw new TypeError(`Page param "${key}" must be a string`);
+  }
+  if (typeof transform === "function") {
+    return transform(value);
+  }
+  return value;
+}
