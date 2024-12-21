d078142f42306dcb18c38e8ffe8e032909dfb272
geforseN <geforsenjs@gmail.com>
2024-12-21 22:13:33 +0700

    refactor(components)!: [game-card] simplify

--- layers/card-game/layers/card/game-card.vue
+++ layers/card-game/layers/card/game-card.vue
@@ -1,20 +1,17 @@
 <template>
   <div
     :style="{
-      backgroundImage: `url(https://deckofcardsapi.com/static/img/${cardId}.png)`,
+      backgroundImage: `url(https://deckofcardsapi.com/static/img/${id}.png)`,
     }"
     class="h-[77px] w-[55px] rounded border-2 border-black bg-black bg-cover xxs:h-[96px] xxs:w-[69px] md:h-[105px] md:w-[75px] lg:h-[127px] lg:w-[91px]"
   />
 </template>
 <script setup lang="ts">
-import type { Rank, Suit } from "$/card-game/types";
-import { makeCardId } from "$/card-game/utils/card/make-card-id";
 import { computed } from "vue";
+import { makeCardId } from "$/card-game/utils/card/make-card-id";
+import type { Card } from "@durak-game/durak-dts";
 
-const props = defineProps<{
-  rank: Rank;
-  suit: Suit;
-}>();
+const { rank, suit } = defineProps<Card>();
 
-const cardId = computed(() => makeCardId(props.rank, props.suit));
+const id = computed(() => makeCardId(rank, suit));
 </script>
