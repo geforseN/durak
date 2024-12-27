3d93069805273511c5dd502d16e440a89a54b8f5
geforseN <geforsenjs@gmail.com>
2024-12-27 23:03:19 +0700

    refactor(routes): use durak-game-with-websocket

--- app/routes/games/[gameId].vue
+++ app/routes/games/[gameId].vue
@@ -1,10 +1,10 @@
 <template>
   <Suspense>
-    <durak-game :id="gameId" />
+    <durak-game-with-websocket :game-id />
   </Suspense>
 </template>
 <script setup lang="ts">
-import DurakGame from "$/card-game/components/_durak-game.vue";
+import DurakGameWithWebsocket from "$/card-game/components/_durak-game-with-websocket.vue";
 import { getRouteStringParam } from "@/router/utils";
 
 const gameId = getRouteStringParam("gameId");
