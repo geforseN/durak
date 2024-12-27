9c785126669461c0656bb3874b6b42d500910ab8
geforseN <geforsenjs@gmail.com>
2024-12-27 22:47:41 +0700

    feat(components)!: [with-self-allowed-message-hint] add shadow

--- layers/card-game/layers/self/as-allowed/with-self-allowed-message-hint.vue
+++ layers/card-game/layers/self/as-allowed/with-self-allowed-message-hint.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex border border-info rounded w-fit items-center justify-center gap-2 px-2 py-1">
+  <div class="flex drop-shadow-lg shadow-info/30 shadow-lg border border-info rounded w-fit items-center justify-center gap-2 px-2 py-1">
     <span class="flex gap-2 select-none items-center">
       <i-rivet-icons-happy />
       Время твоего хода
