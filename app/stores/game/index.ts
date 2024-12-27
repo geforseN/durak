4ab4e83d3c35015a8ce0207e1a0d01032f308a52
geforseN <geforsenjs@gmail.com>
2024-12-27 15:50:13 +0700

    chore(stores)!: [game] delete

--- app/stores/game/index.ts
+++
@@ -1,4 +0,0 @@
-export { useGameStateStore } from "@/stores/game/game.state.store";
-export { useGameDeskStore } from "@/stores/game/desk.store";
-export { useGameSelfStore } from "@/stores/game/self.store";
-export { useGamePlayersStore } from "@/stores/game/players.store";
