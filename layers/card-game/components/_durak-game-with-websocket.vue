631c09108d9ad7a978fe3bb3389f94e4e66ecfa4
geforseN <geforsenjs@gmail.com>
2024-12-27 22:43:12 +0700

    chore(components): [_durak-game-with-websocket] delete commented

--- layers/card-game/components/_durak-game-with-websocket.vue
+++ layers/card-game/components/_durak-game-with-websocket.vue
@@ -107,39 +107,4 @@ function onCardDrop(_: DragEvent, card: Card, slotIndex: number) {
     return;
   }
 }
-
-// socket.on("connect_error", (err) => {
-//   // the reason of the error, for example "xhr poll error"
-//   console.log(err.message);
-
-//   // some additional description, for example the status code of the initial HTTP response
-//   console.log(err.description);
-
-//   // some additional context, for example the XMLHttpRequest object
-//   console.log(err.context);
-// });
-
-// socket.on('game::state::restore', (payload) => {
-//   console.log({ payload })
-//   asyncGameState.resolve(payload.state)
-// })
-// const gameState = await asyncGameState.promise
-
-// socket.on("talon::madeDistribution")
-// const talon = useNoCardCountTalon(gameState.talon);
-
-// desk.on("card::clear", deskStore.clear)
-// desk.on("card::receive", deskStore.insertCard)
-// discard.on("cards::receive", (discard) => )
-// talon.on("distribution::make", )
-// self.on("cards::receive", playersStore.putCardsToPlayer)
-// self.on("card::drop-attempt", playersStore.putCardsToPlayer)
-// enemy.on("cards::receive", playersStore.putCardsToPlayer)
-// enemy.on("card::drop-attempt", playersStore.putCardsToPlayer)
-// player.on("changedKind", playersStore.changePlayerKind)
-
-//  on game state restore =>
-// self must have cards with isTrump
-// this can be done through talon.trumpCard
-// this should be done on server
 </script>
