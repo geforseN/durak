f912ea19c5db9533fa0afd2e51d09206d1fdc3b9
geforseN <geforsenjs@gmail.com>
2024-12-27 22:25:45 +0700

    chore(components): [self-card...] delete commented

--- layers/card-game/layers/self/as-allowed/card/self-attacker-card_.vue
+++ layers/card-game/layers/self/as-allowed/card/self-attacker-card_.vue
@@ -1,7 +1,7 @@
 <template>
   <self-base-card
     ref="selfBaseCard"
-    v-bind="baseCardProps"
+    v-bind="$props"
     class="border-red-600"
     :class="[canBeUsedForAttack && 'outline outline-yellow-500']"
   />
@@ -9,22 +9,14 @@
 <!-- NOTE: this component must be used when self is attacker -->
 <script setup lang="ts">
 import { computed, useTemplateRef } from "vue";
-import SelfBaseCard, { type SelfBaseCardProps } from "../../card/self-base-card.vue";
-// import { injectOrThrow } from "@/utils/vue/inject-or-throw";
-import type { Rank } from "@durak-game/durak-dts";
+import SelfBaseCard, {
+  type SelfBaseCardProps,
+} from "../../card/self-base-card.vue";
 
-const { rank: _, ...baseCardProps } = defineProps<
-  SelfBaseCardProps & { rank: Rank }
->();
+defineProps<SelfBaseCardProps>();
 
-// const gameDeskSlots = injectOrThrow<{
-//   ranks: ComputedRef<Rank[]>;
-// }>("gameDeskSlots");
+const canBeUsedForAttack = computed(() => false);
 
-const canBeUsedForAttack = computed(
-  () => false,
-  // gameDeskSlots.ranks.value.includes(rank),
-);
 const baseCardRef = useTemplateRef("selfBaseCard");
 const element = computed(() => baseCardRef.value?.element);
 
--- layers/card-game/layers/self/as-allowed/card/self-defender-card_.vue
+++ layers/card-game/layers/self/as-allowed/card/self-defender-card_.vue
@@ -15,35 +15,20 @@
 <!-- NOTE: this component must be used when self is defender -->
 <script setup lang="ts">
 import { computed, useTemplateRef } from "vue";
-import SelfBaseCard, { type SelfBaseCardProps } from "../../card/self-base-card.vue";
-import { powerOfCard } from "$/card-game/utils/card/powers";
+import SelfBaseCard, {
+  type SelfBaseCardProps,
+} from "../../card/self-base-card.vue";
 import type { Card } from "@durak-game/durak-dts";
 
-const { isTrump, rank, suit, ...baseCardProps } = defineProps<
+const { isTrump, rank, suit, ...props } = defineProps<
   SelfBaseCardProps &
     Card & {
       isTrump: boolean;
     }
 >();
 
-const power = computed(() => powerOfCard({ rank, suit }));
-
-// const unbeatenSlots = computed(() => gameDesk.slots.unbeaten);
-
 const canBeUsedForDefense = computed(() => {
-  if (isTrump) {
-    return false;
-    return (
-      unbeatenSlots.value.includes((slot) => slot.hasNoTrumpCard()) ||
-      unbeatenSlots.value
-        .filter((slot) => slot.hasTrumpCard())
-        .some((slot) => slot.attackCard.power < power.value)
-    );
-  }
   return false;
-  return unbeatenSlots.value
-    .filter((slot) => slot.attackCard === suit)
-    .some((slot) => powerOfCard(slot.attackCard) < power);
 });
 
 const baseCardRef = useTemplateRef("selfBaseCard");
@@ -53,15 +38,5 @@ defineExpose({ element });
 
 const canBeUsedForTransferMove = computed(() => {
   return false;
-  // FIXME: implement, add tests
-  // gameDeskStore.allowsTransferMove &&
-  // gameDeskStore.ranks[0] === card.rank
-  //    const deskAllowsTransferMove = computed(() => {
-  //     if (hasDefendedSlot.value || isEmpty.value || !firstUnbeatenSlotRank.value)
-  //       return false;
-  //     return unbeatenSlots.value.every(
-  //       (slot) => slot.attackCard.rank === firstUnbeatenSlotRank.value,
-  //     );
-  // });
 });
 </script>
