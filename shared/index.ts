76c3648dfe5eee335132eb9b426aa288565541d9
geforseN <geforsenjs@gmail.com>
2024-12-28 14:54:30 +0700

    chore(shared): delete
    not working that way, need to use git submodule

--- shared/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './utils.js'
-export type * from './types.js'
-export type * as DurakGameSocket from "./socket-io-types.js";
--- shared/utils.ts
+++
@@ -1,8 +0,0 @@
-import { playerKinds } from "./const";
-import type { PlayerKind } from "./types";
-
-export function isPlayerKind(kind: string | PlayerKind): kind is PlayerKind {
-  return playerKinds.includes(kind as PlayerKind);
-}
-
-
