a4be6fc7a4ac5af73dc0fd0d2c46dfca176f6184
geforseN <geforsenjs@gmail.com>
2025-02-05 17:22:05 +0700

    fix(routes): [utils] ignore useRoute type error

--- app/router/utils.ts
+++ app/router/utils.ts
@@ -9,6 +9,7 @@ export function getRouteStringParam<T>(
   key: string,
   transform?: (value: string) => T,
 ) {
+  // @ts-expect-error failed to type useRoute
   const value = useRoute().params[key];
   if (typeof value !== "string") {
     throw new TypeError(`Page param "${key}" must be a string`);
