c542863478f5ba31e1c9141f65fabf6c35175c65
geforseN <geforsenjs@gmail.com>
2025-02-06 13:24:28 +0700

    refactor(utils): [env] delete `requireEnv`

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,40 +1,17 @@
 /// <reference types="vite/client" />
 
-export function requireEnv<
-  T extends ImportMeta["env"],
-  K extends keyof T & string,
->(key: K, env: T, transform?: undefined): string;
-
-export function requireEnv<
-  T extends ImportMeta["env"],
-  K extends keyof T & string,
-  V,
->(key: K, env: T, transform: (value: T[K]) => V): V;
-
-export function requireEnv<
-  T extends ImportMeta["env"],
-  K extends keyof T & string,
-  V,
->(key: K, env: T, transform?: (value: T[K]) => V): V {
-  const value = env[key];
-  if (value === undefined) {
-    throw new Error(`${key} environment variable is equal to undefined`);
-  }
-  if (typeof value !== "string") {
-    throw new Error(
-      `${key} environment variable expected to be typeof string, received ${typeof value}`,
-    );
-  }
-  if (typeof transform === "function") {
-    return transform(value);
-  }
-  return value;
-}
-
 export function forEnv<T extends ImportMeta["env"]>(env: T) {
   return {
     require<K extends keyof T & string>(key: K) {
-      const value = requireEnv(key, env);
+      const value = env[key];
+      if (value === undefined) {
+        throw new Error(`${key} environment variable is equal to undefined`);
+      }
+      if (typeof value !== "string") {
+        throw new Error(
+          `${key} environment variable expected to be typeof string, received ${typeof value}`,
+        );
+      }
 
       return {
         value() {
